87e22ed5c0feb88a3a76b99241055ee1ee194062
Alec Roelke <alec.roelke@gmail.com>
2015-06-15 12:56:42 -0400

    Added FilterGroup to begin expansion of the filter's capabilities.

---
+++ MTG Deck Editor/src/gui/filter/FilterGroup.java
@@ -0,0 +1,30 @@
+package gui.filter;
+
+@SuppressWarnings("serial")
+public class FilterGroup extends FilterPanel
+{
+	public FilterGroup() {
+	}
+	@Override
+	public void setContent(String content)
+	{
+	}
+
+	@Override
+	public CardFilter getFilter()
+	{
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty()
+	{
+		return false;
+	}
+
+	@Override
+	public String repr()
+	{
+		return null;
+	}
+}
