7dbb8d333c2567688ee29e6d4f280e18ba963c96
Alec Roelke <alec.roelke@gmail.com>
2015-10-06 12:41:58 -0400

    Began restructuring undo/redo code.

---
+++ MTG Deck Editor/src/gui/editor/UndoableAction.java
@@ -0,0 +1,16 @@
+package gui.editor;
+
+/**
+ * TODO: Use this interface for undo/redo instead of the more clunky DeckAction
+ * - Create an anonymous class with the appropriate edits to the deck implemented
+ * in undo() and redo()
+ * TODO: Comment this class
+ * @author Alec
+ *
+ */
+public interface UndoableAction
+{
+	public void undo();
+	
+	public void redo();
+}
