f6e45eb704678c2705dafb2fdfbe55f2efe3d06b
Alec Roelke <alec.roelke@gmail.com>
2016-04-28 18:00:30 -0400

    Add task for hand calculation feature.

--- MTG Deck Editor/src/editor/gui/editor/EditorFrame.java
+++ MTG Deck Editor/src/editor/gui/editor/EditorFrame.java
@@ -99,6 +99,7 @@ import editor.gui.TableMouseAdapter;
  * TODO: Add a second table to the main panel showing commander/sideboard/extra cards
  * TODO: Add a "priority" for each category (that is editable and persistent) that can be used to sort categories
  * TODO: Indicate average CMC of each category
+ * TODO: Add something for calculating probability for multiple categories at once
  * 
  * @author Alec Roelke
  */
