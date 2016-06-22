d727521730e68e8b97dbee8c8838685da8f25729
Alec Roelke <alec.roelke@gmail.com>
2016-06-22 15:32:05 -0400

    Move a brace in CardList so it's correct.

--- MTG Deck Editor/src/editor/gui/CardList.java
+++ MTG Deck Editor/src/editor/gui/CardList.java
@@ -39,7 +39,8 @@ public class CardList extends JList<String>
 	{
 		super();
 		cards = c;
-		setSelectionModel(new DefaultListSelectionModel() {
+		setSelectionModel(new DefaultListSelectionModel()
+		{
 			{
 				setSelectionMode(SINGLE_SELECTION);
 			}
