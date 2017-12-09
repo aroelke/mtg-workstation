8e66dde16c8c9be4d362225d646119381eae1885
Alec Roelke <alec.roelke@gmail.com>
2017-12-09 01:36:20 -0500

    Remove potential for createFilter to return null

--- src/editor/filter/FilterFactory.java
+++ src/editor/filter/FilterFactory.java
@@ -228,7 +228,7 @@ public abstract class FilterFactory
 		case TAGS:
 			return new TagsFilter();
 		case DEFAULTS:
-			return null;
+			return new BinaryFilter(true);
 		case NONE:
 			return new BinaryFilter(false);
 		case ALL:
