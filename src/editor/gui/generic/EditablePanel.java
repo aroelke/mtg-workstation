408444e70cbdf9a242ecba6c5d66e59623e75eb2
Alec Roelke <alec.roelke@gmail.com>
2018-11-07 23:52:23 -0500

    Improve close button and add actions

--- src/editor/gui/generic/EditablePanel.java
+++ src/editor/gui/generic/EditablePanel.java
@@ -1,34 +1,47 @@
 package editor.gui.generic;
 
+import java .awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.OverlayLayout;
 import javax.swing.SwingUtilities;
-import javax.swing.border.BevelBorder;
 import javax.swing.plaf.basic.BasicButtonUI;
 
 import editor.util.MouseListenerFactory;
-import editor.util.UnicodeSymbols;
 
 @SuppressWarnings("serial")
 public class EditablePanel extends JPanel
 {
-    public static final int CLOSE_SIZE = 17;
+    public static final String CLOSE = "close";
+    public static final String EDIT = "edit";
+    public static final String CANCEL = "cancel";
 
     private class CloseButton extends JButton
     {
+        private static final int CLOSE_SIZE = 17;
+
         public CloseButton()
         {
             setPreferredSize(new Dimension(CLOSE_SIZE, CLOSE_SIZE));
@@ -46,6 +59,10 @@ public class EditablePanel extends JPanel
                 (e) -> setBorder(BorderFactory.createLoweredBevelBorder()),
                 (e) -> setBorder(BorderFactory.createRaisedBevelBorder())
             ));
+            addActionListener((e) -> {
+                for (ActionListener listener : listeners)
+                    listener.actionPerformed(new ActionEvent(EditablePanel.this, ActionEvent.ACTION_PERFORMED, CLOSE));
+            });
         }
 
         @Override
@@ -55,10 +72,12 @@ public class EditablePanel extends JPanel
             Graphics2D g2 = (Graphics2D)g.create();
 
             g2.setColor(getModel().isRollover() ? Color.RED : Color.BLACK);
-            String cross = String.valueOf(UnicodeSymbols.MULTIPLY);
-            Rectangle bounds = g2.getFont().createGlyphVector(g2.getFontRenderContext(), cross).getPixelBounds(null, 0, 0);
-            int off = getModel().isPressed() ? 1 : 0;
-            g2.drawString(cross, (int)(getWidth() - bounds.getWidth())/2 + off, getHeight()/2 + (int)bounds.getHeight()/2 + off - 1);
+            g2.setStroke(new BasicStroke(1));
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            if (getModel().isPressed())
+                g2.translate(1, 1);
+            g2.drawLine(getWidth()/3, getHeight()/3, getWidth()*2/3, getHeight()*2/3);
+            g2.drawLine(getWidth()/3, getHeight()*2/3, getWidth()*2/3, getHeight()/3);
             g2.dispose();
         }
     }
@@ -66,6 +85,7 @@ public class EditablePanel extends JPanel
     private JTextField field;
     private JPanel front;
     private JLabel label;
+    private Set<ActionListener> listeners;
 
     public EditablePanel(String title, Component parent)
     {
@@ -86,6 +106,15 @@ public class EditablePanel extends JPanel
         field.setBorder(BorderFactory.createEmptyBorder());
         field.setVisible(false);
         field.addActionListener((e) -> finish(true));
+        field.getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), CANCEL);
+        field.getActionMap().put(CANCEL, new AbstractAction()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                finish(false);
+            }
+        });
         add(field);
 
         label.addMouseListener(MouseListenerFactory.composeListeners(
@@ -100,6 +129,20 @@ public class EditablePanel extends JPanel
                 field.selectAll();
             })
         ));
+
+        addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {}
+
+            @Override
+            public void focusLost(FocusEvent e)
+            {
+                finish(false);
+			}
+
+        });
+
+        listeners = new HashSet<>();
     }
 
     public EditablePanel(String title)
@@ -107,11 +150,27 @@ public class EditablePanel extends JPanel
         this(title, null);
     }
 
+    public void addActionListener(ActionListener listener)
+    {
+        listeners.add(listener);
+    }
+
     private void finish(boolean commit)
     {
-        label.setText(field.getText());
         front.setVisible(true);
         field.setVisible(false);
         field.transferFocusUpCycle();
+        if (commit)
+        {
+            label.setText(field.getText());
+            for (ActionListener listener : listeners)
+                listener.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, EDIT));
+        }
+        else
+        {
+            field.setText(label.getText());
+            for (ActionListener listener : listeners)
+                listener.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, CANCEL));
+        }
     }
 }
