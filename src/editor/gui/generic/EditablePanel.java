ec5828bacc4c88ab27b6f28f94f2695a3b284eff
Alec Roelke <alec.roelke@gmail.com>
2018-11-22 00:57:27 -0500

    Comment EditablePanel

--- src/editor/gui/generic/EditablePanel.java
+++ src/editor/gui/generic/EditablePanel.java
@@ -32,19 +32,43 @@ import javax.swing.plaf.basic.BasicButtonUI;
 import editor.util.MouseListenerFactory;
 
 /**
- * TODO
+ * This class represents a panel with a line of text that can be edited by
+ * double-clicking on it. It also contains a box with an "X" in it to the right
+ * of the text that can be used to remove the panel.
+ * 
+ * @author Alec Roelke
  */
 @SuppressWarnings("serial")
 public class EditablePanel extends JPanel
 {
+    /**
+     * String signaling the event of the close butting being clicked.
+     */
     public static final String CLOSE = "close";
+    /**
+     * String signaling the event of the text being changed.
+     */
     public static final String EDIT = "edit";
+    /**
+     * String signaling the event of the text change being canceled.
+     */
     public static final String CANCEL = "cancel";
 
+    /**
+     * This class represents a button with an "X" on it that might be used to
+     * indicate the ability to close something. The button appears depressed
+     * while the mouse is held down on it.
+     */
     private class CloseButton extends JButton
     {
+        /**
+         * Size of the "X".
+         */
         private static final int CLOSE_SIZE = 17;
 
+        /**
+         * Create a new CloseButton.
+         */
         public CloseButton()
         {
             setPreferredSize(new Dimension(CLOSE_SIZE, CLOSE_SIZE));
@@ -85,12 +109,38 @@ public class EditablePanel extends JPanel
         }
     }
 
+    /**
+     * Field containing the text while it is being edited.
+     */
     private JTextField field;
+    /**
+     * Panel containing the static text and close button.
+     */
     private JPanel front;
+    /**
+     * Label containing the static text.
+     */
     private JLabel label;
+    /**
+     * Listeners that are listening for changes to the text or the close button
+     * being pressed.
+     */
     private Set<ActionListener> listeners;
+    /**
+     * Old text before a change was made, initially <code>null</code>. This allows
+     * listeners to make changes based on the old text as well as the new text.
+     */
     private String old;
 
+    /**
+     * Create a new EditablePanel. This panel is "transparent" to mouse events
+     * in that it should not prevent its container's mouse events from happening
+     * if the mouse is over it.
+     * 
+     * @param title initial text to display
+     * @param parent parent component to forward mouse events to, or <code>null</code>
+     * if the panel should not forward mouse events.
+     */
     public EditablePanel(String title, Component parent)
     {
         super();
@@ -151,16 +201,35 @@ public class EditablePanel extends JPanel
         listeners = new HashSet<>();
     }
 
+    /**
+     * Create a new EditablePanel that does not forward mouse events.
+     * 
+     * @param title initial text to display
+     */
     public EditablePanel(String title)
     {
         this(title, null);
     }
 
+    /**
+     * Add a new listener for changes to this EditablePanel.
+     * 
+     * @param listener listener to add
+     */
     public void addActionListener(ActionListener listener)
     {
         listeners.add(listener);
     }
 
+    /**
+     * Finish editing the text to display.  An action event is fired with either
+     * the #{@link #EDIT} command to signify the text changed or the {@link #CANCEL}
+     * command to signal it has not.
+     * 
+     * @param commit <code>true</code> if the text to display should be updated
+     * with the contents of the text field, and <code>false</code> if it should not
+     * be.
+     */
     private void finish(boolean commit)
     {
         front.setVisible(true);
@@ -181,16 +250,30 @@ public class EditablePanel extends JPanel
         }
     }
 
+    /**
+     * @return the previous text before editing, or <code>null</code> if it hasn't
+     * been edited yet.
+     */
     public String getOldTitle()
     {
         return old;
     }
 
+    /**
+     * @return the current text being displayed by this EditablePanel.
+     */
     public String getTitle()
     {
         return label.getText();
     }
 
+    /**
+     * Set the text to display.  This does update the return value of the old
+     * title to the text that was displayed before this was called.
+     * 
+     * @param title new text to display
+     * @see #getOldTitle()
+     */
     public void setTitle(String title)
     {
         old = label.getText();
