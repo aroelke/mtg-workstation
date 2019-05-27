79bbd36228ec8e21a514ece437508995b3beb256
Alec Roelke <alec.roelke@gmail.com>
2019-05-26 23:45:06 -0400

    Compact FilterAttribute's comments

--- src/editor/filter/FilterAttribute.java
+++ src/editor/filter/FilterAttribute.java
@@ -19,118 +19,62 @@ import java.util.stream.Collectors;
  */
 public enum FilterAttribute
 {
-    /**
-     * Filter by artist name.
-     */
+    /** Filter by artist name. */
     ARTIST("Artist", (a) -> new TextFilter(a, Card::artist)),
-    /**
-     * Filter by block.
-     */
+    /** Filter by block. */
     BLOCK("Block", (a) -> new BlockFilter()),
-    /**
-     * Filter by card number.
-     */
+    /** Filter by card number. */
     CARD_NUMBER("Card Number", (a) -> new NumberFilter(a, (c) -> c.number().stream().map((v) -> Double.valueOf(v.replace("--", "0").replaceAll("[\\D]", ""))).collect(Collectors.toList()))),
-    /**
-     * Filter by card type.
-     */
+    /** Filter by card type. */
     CARD_TYPE("Card Type", (a) -> new CardTypeFilter()),
-    /**
-     * Filter by converted mana cost.
-     */
+    /** Filter by converted mana cost. */
     CMC("CMC", (a) -> new NumberFilter(a, Card::cmc)),
-    /**
-     * Filter by color.
-     */
+    /** Filter by color. */
     COLOR("Color", (a) -> new ColorFilter(a, Card::colors)),
-    /**
-     * Filter by color identity.
-     */
+    /** Filter by color identity. */
     COLOR_IDENTITY("Color Identity", (a) -> new ColorFilter(a, Card::colorIdentity)),
-    /**
-     * Filter by expansion.
-     */
+    /** Filter by expansion. */
     EXPANSION("Expansion", (a) -> new ExpansionFilter()),
-    /**
-     * Filter by flavor text.
-     */
+    /** Filter by flavor text. */
     FLAVOR_TEXT("Flavor Text", (a) -> new TextFilter(a, Card::normalizedFlavor)),
-    /**
-     * Filter by format legality.
-     */
+    /** Filter by format legality. */
     FORMAT_LEGALITY("Format Legality", (a) -> new LegalityFilter()),
-    /**
-     * Filter by layout.
-     */
+    /** Filter by layout. */
     LAYOUT("Layout", (a) -> new LayoutFilter()),
-    /**
-     * Filter by loyalty.
-     */
+    /** Filter by loyalty. */
     LOYALTY("Loyalty", (a) -> new VariableNumberFilter(a, (Card c) -> c.loyalty().stream().map((l) -> (double)l.value).collect(Collectors.toList()), Card::loyaltyVariable)),
-    /**
-     * Filter by mana cost.
-     */
+    /** Filter by mana cost. */
     MANA_COST("Mana Cost", (a) -> new ManaCostFilter()),
-    /**
-     * Filter by name.
-     */
+    /** Filter by name. */
     NAME("Name", (a) -> new TextFilter(a, Card::normalizedName)),
-    /**
-     * Filter by power.
-     */
+    /** Filter by power. */
     POWER("Power", (a) -> new VariableNumberFilter(a, (c) -> c.power().stream().map((p) -> p.value).collect(Collectors.toList()), Card::powerVariable)),
-    /**
-     * Filter by printed text.
-     */
+    /** Filter by printed text. */
     PRINTED_TEXT("Printed Text", (a) -> new TextFilter(a, Card::normalizedPrinted)),
-    /**
-     * Filter by printed type line.
-     */
+    /** Filter by printed type line. */
     PRINTED_TYPES("Printed Type Line", (a) -> new TextFilter(a, Card::printedTypes)),
-    /**
-     * Filter by rarity.
-     */
+    /** Filter by rarity. */
     RARITY("Rarity", (a) -> new RarityFilter()),
-    /**
-     * Filter by rules (Oracle) text.
-     */
+    /** Filter by rules (Oracle) text. */
     RULES_TEXT("Rules Text", (a) -> new TextFilter(a, Card::normalizedOracle)),
-    /**
-     * Filter by subtype.
-     */
+    /** Filter by subtype. */
     SUBTYPE("Subtype", (a) -> new SubtypeFilter()),
-    /**
-     * Filter by supertype.
-     */
+    /** Filter by supertype. */
     SUPERTYPE("Supertype", (a) -> new SupertypeFilter()),
-    /**
-     * Filter by user-defined tags.
-     */
+    /** Filter by user-defined tags. */
     TAGS("Tags", (a) -> new TagsFilter()),
-    /**
-     * Filter by toughness.
-     */
+    /** Filter by toughness. */
     TOUGHNESS("Toughness", (a) -> new VariableNumberFilter(a, (c) -> c.toughness().stream().map((p) -> p.value).collect(Collectors.toList()), Card::toughnessVariable)),
-    /**
-     * Filter by type line.
-     */
+    /** Filter by type line. */
     TYPE_LINE("Type Line", (a) -> new TypeLineFilter()),
 
-    /**
-     * No filter applied.
-     */
+    /** No filter applied. */
     ANY("<Any Card>", (a) -> new BinaryFilter(true)),
-    /**
-     * Filter all cards.
-     */
+    /** Filter all cards. */
     NONE("<No Card>", (a) -> new BinaryFilter(false)),
-    /**
-     * Filter using one of the predefined filters.  This should not be used to create a filter.
-     */
+    /** Filter using one of the predefined filters.  This should not be used to create a filter. */
     DEFAULTS("Defaults", null),
-    /**
-     * Group of filters.  This should not be used to create a filter.
-     */
+    /** Group of filters.  This should not be used to create a filter. */
     GROUP("Group", null);
 
     /**
