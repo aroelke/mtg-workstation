0289a1b1312c8efd37848aa14af130db7c48c537
Alec Roelke <alec.roelke@gmail.com>
2020-05-06 16:59:25 -0400

    Improve ManaCost weight calcluation efficiency

--- src/editor/database/characteristics/ManaCost.java
+++ src/editor/database/characteristics/ManaCost.java
@@ -101,7 +101,7 @@ public class ManaCost extends AbstractList<ManaSymbol> implements Comparable<Man
         cost = Collections.unmodifiableList(symbols);
         weights = ManaSymbol.createWeights();
         for (ManaSymbol sym : cost)
-            for (ManaType col : weights.keySet())
+            for (ManaType col : sym.colorWeights().keySet())
                 weights.compute(col, (k, v) -> sym.colorWeights().get(k) + v);
     }
 
