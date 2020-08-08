84b7d3f7f3963ebca714ead0c3763e2a8cc4aa8e
Alec Roelke <alec.roelke@gmail.com>
2020-08-08 19:29:31 -0400

    Add documentation to card import handler

--- src/main/java/editor/gui/ccp/handler/CardImportHandler.java
+++ src/main/java/editor/gui/ccp/handler/CardImportHandler.java
@@ -12,12 +12,23 @@ import editor.database.card.Card;
 import editor.gui.ccp.data.DataFlavors;
 import editor.gui.editor.EditorFrame;
 
+/**
+ * Import handler for importing a list of single copies of cards.
+ */
 @SuppressWarnings("serial")
 public class CardImportHandler extends TransferHandler implements ImportHandler
 {
+    /** Editor frame that is importing the cards. */
     private final EditorFrame editor;
+    /** ID of the list to import cards to. */
     private final int id;
 
+    /**
+     * Create a new card import handler.
+     * 
+     * @param e editor frame containing the deck to import cards into
+     * @param i ID of the list in the deck to import into
+     */
     public CardImportHandler(EditorFrame e, int i)
     {
         editor = e;
