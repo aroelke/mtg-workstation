0c27abd1cdbc26750dc7b7c9beaa0bc0593aa9af
Alec Roelke <alec.roelke@gmail.com>
2020-08-08 19:14:13 -0400

    Add documentation to inventory exporter

--- src/main/java/editor/gui/ccp/handler/InventoryExportHandler.java
+++ src/main/java/editor/gui/ccp/handler/InventoryExportHandler.java
@@ -10,11 +10,22 @@ import javax.swing.TransferHandler;
 import editor.database.card.Card;
 import editor.gui.ccp.data.CardTransferData;
 
+/**
+ * This class is a handler for expording copied data from the inventory. It only
+ * supports copying single copies of cards.
+ */
 @SuppressWarnings("serial")
 public class InventoryExportHandler extends TransferHandler
 {
+    /** Function for generating the list of cards to export. */
     private final Supplier<? extends Collection<? extends Card>> cards;
 
+    /**
+     * Create a new inventory export handler that determines the cards to export
+     * using the given function.
+     * 
+     * @param c supplier of the list of cards to export
+     */
     public InventoryExportHandler(Supplier<? extends Collection<? extends Card>> c)
     {
         cards = c;
