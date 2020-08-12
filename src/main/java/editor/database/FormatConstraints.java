ce722bbaddd5c0a992a0f806fc595aa3a5aa99cc
Alec Roelke <alec.roelke@gmail.com>
2020-08-12 19:47:22 -0400

    Add documentation to FormatConstraints

--- src/main/java/editor/database/FormatConstraints.java
+++ src/main/java/editor/database/FormatConstraints.java
@@ -6,8 +6,16 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+/**
+ * This class represents the constraints on building a deck for a particular format.
+ * 
+ * @author Alec Roelke
+ */
 public class FormatConstraints
 {
+    /**
+     * Mapping of format names onto their deckbulding constraints.
+     */
     public static final Map<String, FormatConstraints> CONSTRAINTS = Map.ofEntries(
         new SimpleImmutableEntry<>("brawl", new FormatConstraints(60, true, 1, true)),
         new SimpleImmutableEntry<>("commander", new FormatConstraints(100, true, 1, true)),
@@ -24,8 +32,14 @@ public class FormatConstraints
         new SimpleImmutableEntry<>("vintage", new FormatConstraints())
     );
 
+    /**
+     * List of supported format names, in alphabetical order.
+     */
     public static final List<String> FORMAT_NAMES = CONSTRAINTS.keySet().stream().sorted().collect(Collectors.toList());
 
+    /**
+     * List of types of each of the deckbuilding constraints.
+     */
     public static final List<Class<?>> CLASSES = List.of(
         String.class,
         Integer.class,
@@ -34,6 +48,13 @@ public class FormatConstraints
         Boolean.class
     );
 
+    /**
+     * The name of each type of deckbuilding constraint: name of the format,
+     * number of cards allowed in the deck, whether that number is a minimum
+     * number or exact number, number of copies of any card allowed in a deck
+     * (not counting restricted cards, basic lands, or other cards that ignore
+     * this restriction), and whether or not the format has a commander.
+     */
     public static final List<String> DATA_NAMES = List.of(
         "Name",
         "Deck Size",
@@ -42,11 +63,24 @@ public class FormatConstraints
         "Has Commander?"
     );
 
+    /** Number of cards the deck should have. */
     public final int deckSize;
+    /** Whether {@link #deckSize} represents an exact count or minimum. */
     public final boolean isExact;
+    /** Maximum number of copies of a card in a deck. */
     public final int maxCopies;
+    /** Whether or not the format has a commander. */
     public final boolean hasCommander;
     
+    /**
+     * Create a new set of deckbuilding constraints.  Since this is immutable and
+     * not customizable by the user, this constructor isn't visible outside this class.
+     * 
+     * @param size number of cards in a deck
+     * @param exact whether or not <code>size</code> is an exact count
+     * @param copies number of copies of a card
+     * @param commander whether or not the format has a commander
+     */
     private FormatConstraints(int size, boolean exact, int copies, boolean commander)
     {
         deckSize = size;
@@ -55,11 +89,20 @@ public class FormatConstraints
         hasCommander = commander;
     }
 
+    /**
+     * Create a default set of deckbuilding constraints, which is for a 60-card-
+     * minimum-sized deck with 4 copies of any card and no commander.
+     */
     public FormatConstraints()
     {
         this(60, false, 4, false);
     }
 
+    /**
+     * @param name name of the format
+     * @return An array containig the elements of this set of deckbuilding constraints
+     * with the name of the format prepended, in the order specified by {@link #DATA_NAMES}.
+     */
     public Object[] toArray(String name)
     {
         return new Object[] { name, deckSize, isExact, maxCopies, hasCommander };
