f8d6a2053b7fdce3c0c2057d959bc0ddd21fe2c7
Alec Roelke <alec.roelke@gmail.com>
2021-07-15 17:29:31 -0400

    Properly comment StringUtils

--- src/main/java/editor/util/StringUtils.java
+++ src/main/java/editor/util/StringUtils.java
@@ -1,7 +1,20 @@
 package editor.util;
 
+/**
+ * This interface contains some useful string utilities to make formatting easier.
+ * 
+ * @author Alec Roelke
+ */
 public interface StringUtils
 {
+    /**
+     * Format a double with the desired precision as if with %f, unless it's a whole number,
+     * in which case forgo the decimal place.
+     * 
+     * @param n number to format
+     * @param precision precision to use if the number is fractional
+     * @return The formatted string.
+     */
     static String formatDouble(double n, int precision)
     {
         if (n == (int)n)
