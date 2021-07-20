418e7b7dc634eec7c1660b1de0ec604a136481c9
Alec Roelke <alec.roelke@gmail.com>
2021-07-20 10:18:12 -0400

    Improve colors of bar plot

--- src/main/java/editor/gui/editor/EditorFrame.java
+++ src/main/java/editor/gui/editor/EditorFrame.java
@@ -524,6 +524,7 @@ public class EditorFrame extends JInternalFrame
      */
     private JTabbedPane listTabs;
     private DefaultCategoryDataset manaCurve;
+    private StackedBarRenderer manaCurveRenderer;
     /**
      * Label showing the median mana value of nonland cards in the deck.
      */
@@ -855,10 +856,11 @@ public class EditorFrame extends JInternalFrame
 
         manaCurve = new DefaultCategoryDataset();
         landDrops = new DefaultCategoryDataset();
-        StackedBarRenderer manaCurveRenderer = new StackedBarRenderer();
+        manaCurveRenderer = new StackedBarRenderer();
         manaCurveRenderer.setBarPainter(new StandardBarPainter());
         manaCurveRenderer.setDefaultToolTipGenerator(new StandardCategoryToolTipGenerator("{0}: {2}", new DecimalFormat()));
         manaCurveRenderer.setDrawBarOutline(true);
+        manaCurveRenderer.setDefaultOutlinePaint(Color.BLACK);
         manaCurveRenderer.setShadowVisible(false);
         LineAndShapeRenderer landRenderer = new LineAndShapeRenderer();
         landRenderer.setDefaultItemLabelGenerator(new StandardCategoryItemLabelGenerator());
@@ -2683,6 +2685,29 @@ public class EditorFrame extends JInternalFrame
 
         manaCurve.clear();
         landDrops.clear();
+        switch (sectionsBox.getItemAt(sectionsBox.getSelectedIndex()))
+        {
+        case "Color":
+            manaCurveRenderer.setSeriesPaint(0, new Color(203, 198, 193)); // Colorless (gray)
+            manaCurveRenderer.setSeriesPaint(1, new Color(248, 246, 216)); // White
+            manaCurveRenderer.setSeriesPaint(2, new Color(193, 215, 233)); // Blue
+            manaCurveRenderer.setSeriesPaint(3, new Color(186, 177, 171)); // Black
+            manaCurveRenderer.setSeriesPaint(4, new Color(228, 153, 119)); // Red
+            manaCurveRenderer.setSeriesPaint(5, new Color(163, 192, 149)); // Green
+            manaCurveRenderer.setSeriesPaint(6, new Color(204, 166, 82));  // Multicolored
+            break;
+        case "Card Type":
+            manaCurveRenderer.setSeriesPaint(0, new Color(163, 192, 149)); // Creature (same as green)
+            manaCurveRenderer.setSeriesPaint(1, new Color(203, 198, 193)); // Artifact (same as colorless)
+            manaCurveRenderer.setSeriesPaint(2, new Color(248, 246, 216)); // Enchantment (same as white)
+            manaCurveRenderer.setSeriesPaint(3, new Color(215, 181, 215)); // Planeswalker (purple)
+            manaCurveRenderer.setSeriesPaint(4, new Color(193, 215, 233)); // Instant (same as blue)
+            manaCurveRenderer.setSeriesPaint(5, new Color(228, 153, 119)); // Sorcery (same as red)
+            break;
+        default:
+            manaCurveRenderer.setSeriesPaint(0, new Color(128, 128, 255));
+            break;
+        }
         CardList analyte = analyzeCategoryBox.isSelected() ? deck().current.getCategoryList(analyzeCategoryCombo.getItemAt(analyzeCategoryCombo.getSelectedIndex())) : deck().current;
         if (analyte.total() - lands > 0)
         {
