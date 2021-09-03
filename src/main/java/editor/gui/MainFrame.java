8afa9fb7028e28d6a0c682577287030cd0dd4405
Alec Roelke <alec.roelke@gmail.com>
2021-09-03 16:57:31 -0400

    Move main class to Scala

--- src/main/java/editor/gui/MainFrame.java
+++ src/main/java/editor/gui/MainFrame.java
@@ -272,35 +272,6 @@ public class MainFrame extends JFrame
     }
 
     /**
-     * Entry point for the program. All it does is set the look and feel to the
-     * system one and create the GUI.
-     *
-     * @param args arguments to the program
-     */
-    public static void main(String[] args)
-    {
-        try
-        {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }
-        catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e)
-        {
-            e.printStackTrace();
-        }
-
-        try
-        {
-            Files.createDirectories(SettingsDialog.EDITOR_HOME());
-        }
-        catch (IOException e)
-        {
-            JOptionPane.showMessageDialog(null, "Could not create directory " + SettingsDialog.EDITOR_HOME() + ": " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-        }
-
-        SwingUtilities.invokeLater(() -> new MainFrame(Arrays.stream(args).map(File::new).filter(File::exists).collect(Collectors.toList())).setVisible(true));
-    }
-
-    /**
      * Table displaying the inventory of all cards.
      */
     private CardTable inventoryTable;
---
+++ src/main/scala/editor/EditorApp.scala
@@ -0,0 +1,39 @@
+package editor
+
+import editor.gui.MainFrame
+import editor.gui.settings.SettingsDialog
+
+import java.io.File
+import java.io.IOException
+import java.nio.file.Files
+import javax.swing.JOptionPane
+import javax.swing.SwingUtilities
+import javax.swing.UIManager
+import javax.swing.UnsupportedLookAndFeelException
+import scala.jdk.CollectionConverters._
+
+/**
+ * Main class for starting the program.
+ * @author Alec Roelke
+ */
+object EditorApp {
+  /**
+   * Create a new [[editor.gui.MainFrame]] and open it with the given set of files.
+   * @param files decks to open
+   */
+  @main def main(files: String*) = {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName)
+    } catch {
+      case e @ (_: ClassNotFoundException | _: InstantiationException | _: IllegalAccessException | _: UnsupportedLookAndFeelException | _: Exception) => e.printStackTrace
+    }
+
+    try {
+      Files.createDirectories(SettingsDialog.EditorHome)
+    } catch {
+      case e: IOException => JOptionPane.showMessageDialog(null, s"Could not create directory ${SettingsDialog.EditorHome}: ${e.getMessage}", "Error", JOptionPane.ERROR_MESSAGE)
+    }
+
+    SwingUtilities.invokeLater(() => MainFrame(files.map(File(_)).filter(_.exists)).setVisible(true))
+  }
+}
