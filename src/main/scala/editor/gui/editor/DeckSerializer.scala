df781b412ade1dc4192c2506c736db0d9a25eab1
Alec Roelke <alec.roelke@gmail.com>
2021-11-13 14:01:49 -0500

    Add comments to DeckSerializer

--- src/main/scala/editor/gui/editor/DeckSerializer.scala
+++ src/main/scala/editor/gui/editor/DeckSerializer.scala
@@ -1,51 +1,62 @@
 package editor.gui.editor
 
-import com.google.gson.JsonSerializer
-import com.google.gson.JsonDeserializer
-import editor.collection.deck.Deck
-import java.io.File
-import java.util.NoSuchElementException
-import java.io.FileWriter
-import scala.util.Using
-import editor.gui.MainFrame
-import java.lang.reflect.Type
-import com.google.gson.JsonSerializationContext
-import com.google.gson.JsonObject
 import com.google.gson.JsonArray
-import com.google.gson.JsonElement
 import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
 import com.google.gson.JsonParseException
-import scala.jdk.CollectionConverters._
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.awt.Window
-import java.io.InputStream
-import javax.swing.SwingWorker
-import javax.swing.JDialog
-import java.awt.Dialog
-import javax.swing.JProgressBar
-import javax.swing.JPanel
+import com.google.gson.JsonSerializationContext
+import com.google.gson.JsonSerializer
+import editor.collection.deck.Deck
+import editor.collection.`export`.CardListFormat
+import editor.gui.MainFrame
+import editor.util.ProgressInputStream
+
 import java.awt.BorderLayout
+import java.awt.Component
+import java.awt.Dialog
 import java.awt.FlowLayout
-import javax.swing.JLabel
+import java.awt.Window
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileWriter
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.ObjectInput
+import java.io.ObjectInputStream
+import java.lang.reflect.Type
+import java.nio.file.NoSuchFileException
+import java.text.SimpleDateFormat
+import java.util.NoSuchElementException
+import java.util.concurrent.CancellationException
 import javax.swing.BorderFactory
 import javax.swing.JButton
-import java.io.FileInputStream
-import editor.util.ProgressInputStream
-import java.awt.Component
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.util.concurrent.CancellationException
-import editor.collection.`export`.CardListFormat
-import java.io.ObjectInputStream
-import java.io.ObjectInput
-import java.nio.file.NoSuchFileException
+import javax.swing.JDialog
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.JProgressBar
+import javax.swing.SwingWorker
+import scala.jdk.CollectionConverters._
+import scala.util.Using
 
+/**
+ * Companion object containing global information about serializing decks and for creating new [[DeckSerializer]]s.
+ * @author Alec Roelke
+ */
 object DeckSerializer {
+  /** @return the format used to serialize dates (e.g. for when a card was added to the deck) */
   def ChangelogDateFormat = SimpleDateFormat("MMMM d, yyyy HH:mm:ss")
 
-  def SaveVersion = 3L
-
+  /**
+   * Load a deck from a JSON file.  Display a dialog indicating progress in parsing the file.
+   * 
+   * @param file file to load from
+   * @param parent component doing the loading, for positioning the progress dialog
+   * @return a [[DeckSerializer]] containing the data parsed from the file
+   */
   @throws[DeckLoadException]("if the deck couldn't be loaded (not including user cancellation)")
   def load(file: File, parent: Component) = {
     val worker = LoadWorker(file, parent, (stream) => Using.resource(BufferedReader(InputStreamReader(stream)))(MainFrame.Serializer.fromJson(_, classOf[DeckSerializer])))
@@ -58,6 +69,16 @@ object DeckSerializer {
     }
   }
 
+  /**
+   * Import deck data from a supported non-JSON format. Display a dialog indicating progressin parsing the file.
+   * The file that is opened cannot be saved back to using a normal "Save" action; doing so saves a new JSON file.
+   * Overwriting the old file can only be done using the export feature.
+   * 
+   * @param format format to use to parse the file
+   * @param file file to load from
+   * @param parent component doing the loading, used for positioning the progress dialog
+   * @return a [[DeckSerializer]] containing the data parsed from the file
+   */
   @throws[DeckLoadException]("if the list couldn't be imported (not including user cancellation)")
   def importList(format: CardListFormat, file: File, parent: Component) = {
     val worker = LoadWorker(file, parent, format.parse(_))
@@ -71,11 +92,25 @@ object DeckSerializer {
   }
 }
 
+/**
+ * Structure useful for serializing and deserializing deck data to and from JSON format.  The serialized deck contains the main contents
+ * of the deck, any extra lists included alongside it, any user-defined notes, and the changelog.
+ * 
+ * @constructor create a new deck serializer
+ * @param deck main deck contents
+ * @param sideboards extra lists with the deck
+ * @param notes user-defined notes
+ * @param changelog changes made since the deck was created
+ * @param file file to save the serialized deck to
+ * 
+ * @author Alec Roelke
+ */
 case class DeckSerializer(deck: Deck = Deck(), sideboards: Map[String, Deck] = Map.empty, notes: String = "", changelog: String = "", file: Option[File] = None)
   extends JsonSerializer[DeckSerializer] with JsonDeserializer[DeckSerializer]
 {
   @deprecated def this(d: Deck, s: java.util.Map[String, Deck], n: String, c: String) = this(d, s.asScala.toMap, n, c)
 
+  /** Save the serialized deck to a JSON file, if the file is defined. */
   @throws[IOException]("if the file could not be saved")
   @throws[NoSuchFileException]("if there is no file to save to")
   def save() = file.map((f) => Using.resource(FileWriter(f)){ writer =>
@@ -110,7 +145,14 @@ case class DeckSerializer(deck: Deck = Deck(), sideboards: Map[String, Deck] = M
     DeckSerializer(deck, sideboard.toMap, notes, changelog)
   }
 }
-
+/**
+ * Helper class for loading a deck in the background. Shows a dialog indicating load progress.
+ * 
+ * @constructor create a new background load worker
+ * @param file file to load from
+ * @param parent component doing the loading, for positioning the dialog
+ * @param background function converting the data from the file input stream into a [[DeckSerializer]]
+ */
 private class LoadWorker(file: File, parent: Component, background: (InputStream) => DeckSerializer) extends SwingWorker[DeckSerializer, Integer] {
   private val dialog = JDialog(null, Dialog.ModalityType.APPLICATION_MODAL)
   private val progressBar = JProgressBar(0, file.length.toInt)
