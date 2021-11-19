3e43a87ece2d9bcad8ebbb53e294b2543a1a7740
Alec Roelke <alec.roelke@gmail.com>
2021-11-19 18:43:56 -0500

    Add comments to CardJList

--- src/main/scala/editor/gui/display/CardJList.scala
+++ src/main/scala/editor/gui/display/CardJList.scala
@@ -7,6 +7,14 @@ import javax.swing.ListSelectionModel
 import javax.swing.DefaultListModel
 import editor.gui.settings.SettingsDialog
 
+/**
+ * UI element for displaying a list of cards.  Does not support selection.
+ * 
+ * @constructor create a new card list showing a list of cards
+ * @param cards cards to initially display; can be changed later
+ * 
+ * @author Alec Roelke
+ */
 class CardJList(var cards: Seq[Card] = Seq.empty) extends JList[String] {
   setSelectionModel(new DefaultListSelectionModel {
     setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
