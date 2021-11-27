8f922e6f6cd1f8dec4b6e94fee447b30c46bbe62
Alec Roelke <alec.roelke@gmail.com>
2021-11-27 17:39:48 -0500

    Remove semicolons

--- src/main/scala/editor/gui/generic/EditablePanel.scala
+++ src/main/scala/editor/gui/generic/EditablePanel.scala
@@ -83,51 +83,51 @@ class EditablePanel(t: String, parent: Option[Component] = None) extends JPanel 
   setOpaque(false)
   private var old: Option[String] = None
 
-  private val front = JPanel(BorderLayout());
-  front.setOpaque(false);
+  private val front = JPanel(BorderLayout())
+  front.setOpaque(false)
   private val label = JLabel(t)
-  front.add(label, BorderLayout.WEST);
-  front.add(Box.createHorizontalStrut(2), BorderLayout.CENTER);
-  front.add(CloseButton(), BorderLayout.EAST);
+  front.add(label, BorderLayout.WEST)
+  front.add(Box.createHorizontalStrut(2), BorderLayout.CENTER)
+  front.add(CloseButton(), BorderLayout.EAST)
   add(front)
   label.setFocusable(false)
 
-  private val field = JTextField(t);
-  field.setBorder(BorderFactory.createEmptyBorder);
-  field.setVisible(false);
-  field.addActionListener(_ => finish(true));
-  field.getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), Cancel);
-  field.getActionMap.put(Cancel, new AbstractAction { override def actionPerformed(e: ActionEvent) = finish(false); });
-  add(field);
+  private val field = JTextField(t)
+  field.setBorder(BorderFactory.createEmptyBorder)
+  field.setVisible(false)
+  field.addActionListener(_ => finish(true))
+  field.getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), Cancel)
+  field.getActionMap.put(Cancel, new AbstractAction { override def actionPerformed(e: ActionEvent) = finish(false) })
+  add(field)
 
   label.addMouseListener(MouseListenerFactory.composeListeners(
     MouseListenerFactory.createUniversalListener((e) => {
       parent.foreach(p => p.dispatchEvent(SwingUtilities.convertMouseEvent(e.getComponent, e, p)))
     }),
     MouseListenerFactory.createDoubleClickListener(_ => {
-      front.setVisible(false);
-      field.setText(label.getText);
-      field.setVisible(true);
-      field.requestFocusInWindow();
-      field.selectAll();
+      front.setVisible(false)
+      field.setText(label.getText)
+      field.setVisible(true)
+      field.requestFocusInWindow()
+      field.selectAll()
     })
-  ));
+  ))
 
   field.addFocusListener(new FocusListener {
     override def focusGained(e: FocusEvent) = {}
     override def focusLost(e: FocusEvent) = finish(false)
-  });
+  })
 
   private def finish(commit: Boolean) = {
-    front.setVisible(true);
-    field.setVisible(false);
-    field.transferFocusUpCycle();
+    front.setVisible(true)
+    field.setVisible(false)
+    field.transferFocusUpCycle()
     if (commit) {
       old = Some(label.getText)
-      label.setText(field.getText);
+      label.setText(field.getText)
       listeners.foreach(_.actionPerformed(ActionEvent(this, ActionEvent.ACTION_PERFORMED, Edit)))
     } else {
-      field.setText(label.getText);
+      field.setText(label.getText)
       listeners.foreach(_.actionPerformed(ActionEvent(this, ActionEvent.ACTION_PERFORMED, Cancel)))
     }
   }
