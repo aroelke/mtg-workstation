d264924e92c3b489c3d9645a2ec6f2cff5f7553a
Alec Roelke <alec.roelke@gmail.com>
2021-11-27 09:51:41 -0500

    Add comments to DocumentChangeListener

--- src/main/scala/editor/gui/generic/DocumentChangeListener.scala
+++ src/main/scala/editor/gui/generic/DocumentChangeListener.scala
@@ -1,9 +1,17 @@
 package editor.gui.generic
 
-import javax.swing.event.DocumentListener
 import javax.swing.event.DocumentEvent
+import javax.swing.event.DocumentListener
 
+/**
+ * [[DocumentListener]] that does the same thing for any change to the document.
+ * @author Alec Roelke
+ */
 trait DocumentChangeListener extends DocumentListener {
+  /**
+   * Action to perform when the document changes.
+   * @param e event describing the document change
+   */
   def update(e: DocumentEvent): Unit
 
   override def changedUpdate(e: DocumentEvent) = update(e)
