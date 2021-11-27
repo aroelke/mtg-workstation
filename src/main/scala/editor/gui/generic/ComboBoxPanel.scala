40b515e6797701764d3a3b420979915604a0b21e
Alec Roelke <alec.roelke@gmail.com>
2021-11-27 09:13:30 -0500

    Add comments to ComboBoxPanel

--- src/main/scala/editor/gui/generic/ComboBoxPanel.scala
+++ src/main/scala/editor/gui/generic/ComboBoxPanel.scala
@@ -1,11 +1,21 @@
 package editor.gui.generic
 
-import javax.swing.JPanel
-import javax.swing.BoxLayout
-import javax.swing.Box
-import javax.swing.JComboBox
 import java.awt.event.ItemListener
+import javax.swing.Box
+import javax.swing.BoxLayout
+import javax.swing.JComboBox
+import javax.swing.JPanel
 
+/**
+ * Panel containing a combo box that is sized to fit its contents and vertically
+ * centered in its parent.
+ * 
+ * @constructor create a new centered combo box with a specific set of items
+ * @tparam E type of the combo box's contents
+ * @param items items to put into the combo box
+ * 
+ * @author Alec Roelke
+ */
 class ComboBoxPanel[E <: AnyRef](items: Array[E]) extends JPanel {
   setLayout(BoxLayout(this, BoxLayout.Y_AXIS))
 
@@ -15,9 +25,12 @@ class ComboBoxPanel[E <: AnyRef](items: Array[E]) extends JPanel {
   add(options)
   add(Box.createVerticalGlue)
 
+  /** @param l item listener to add to the combo box */
   def addItemListener(l: ItemListener) = options.addItemListener(l)
 
+  /** @return the currently-selected item in the combo box */
   def getSelectedItem = options.getItemAt(options.getSelectedIndex)
 
+  /** @param item new selected value of the combo box */
   def setSelectedItem(item: E) = options.setSelectedItem(item)
 }
