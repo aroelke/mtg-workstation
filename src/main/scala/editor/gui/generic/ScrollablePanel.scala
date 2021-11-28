fa48ed57b82d925a329e9f6989fcc2efacbca373
Alec Roelke <alec.roelke@gmail.com>
2021-11-27 22:32:30 -0500

    Comment ScrollablePanel

--- src/main/scala/editor/gui/generic/ScrollablePanel.scala
+++ src/main/scala/editor/gui/generic/ScrollablePanel.scala
@@ -6,18 +6,38 @@ import java.awt.LayoutManager
 import java.awt.Dimension
 import java.awt.Rectangle
 
+/**
+ * Collection of constants used for configuring a [[ScrollablePanel]].
+ * @author Alec Roelke
+ */
 object ScrollablePanel {
+  /** Configure a [[ScrollablePanel]] to force its view to track viewport width. */
   val TrackWidth: Byte = 0x01
+  /** Configure a [[ScrollablePanel]] to force its view to track viewport height. */
   val TrackHeight: Byte = 0x02
+  /** Configure a [[ScrollablePanel]] to force its view to track both viewport width and height. */
   val TrackBoth: Byte = (TrackWidth | TrackHeight).toByte
+  /** Configure a [[ScrollablePanel]] to force its view to track neither viewport width nor height. */
   val TrackNeither: Byte = 0x00
 }
 
+/**
+ * A panel that works inside a [[JScrollPane]] or similar component, allowing its contents to stretch larger
+ * than the viewport size with some means of scrolling them (like a scroll bar) to see them all.
+ * 
+ * @constructor create a new scrollable panel that scrolls vertically and/or horizontally and with
+ * a given layout
+ * @param tracking which dimension(s) of the viewport should be tracked
+ * @param layout initial layout of the viewport
+ * 
+ * @author Alec Roelke
+ */
 class ScrollablePanel(tracking: Byte = ScrollablePanel.TrackNeither, layout: LayoutManager = JPanel().getLayout) extends JPanel(layout) with Scrollable {
   @deprecated def this(tracking: Byte) = this(tracking, JPanel().getLayout)
 
   private var preferredScrollableViewportSize = Dimension(0, 0)
 
+  /** @param size new preferred size of the viewport (contents can be different depending on tracking) */
   def setPreferredScrollableViewportSize(size: Dimension) = preferredScrollableViewportSize = size
 
   override def getPreferredScrollableViewportSize = preferredScrollableViewportSize
