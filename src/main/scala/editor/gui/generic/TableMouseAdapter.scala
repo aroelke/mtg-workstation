3e56942eaa325d21ffe8922b67e79d1138d65ad3
Alec Roelke <alec.roelke@gmail.com>
2021-11-28 21:41:08 -0500

    Comment TableMouseAdapter

--- src/main/scala/editor/gui/generic/TableMouseAdapter.scala
+++ src/main/scala/editor/gui/generic/TableMouseAdapter.scala
@@ -1,12 +1,26 @@
 package editor.gui.generic
 
-import java.awt.event.MouseAdapter
-import javax.swing.JTable
-import javax.swing.JPopupMenu
-import java.awt.event.MouseEvent
 import java.awt.event.InputEvent
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import javax.swing.JPopupMenu
+import javax.swing.JTable
 
+/**
+ * Mouse listener for a [[JTable]] that allows for discontiguous selection by holding the Ctrl
+ * key down while clicking.
+ * 
+ * @constructor create a new mouse adapter for a table with a menu
+ * @param table table to listen for mouse events
+ * @param menu menu to open when the menu button is pressed
+ * 
+ * @author Alec Roelke
+ */
 class TableMouseAdapter(table: JTable, menu: JPopupMenu) extends MouseAdapter {
+  /**
+   * Process an event that should generate a popup menu.
+   * @param e mouse event that should generate the popup
+   */
   def popupClick(e: MouseEvent) = if (e.isPopupTrigger) {
     val r = table.rowAtPoint(e.getPoint)
     if (r >= 0 && !table.isRowSelected(r)) {
