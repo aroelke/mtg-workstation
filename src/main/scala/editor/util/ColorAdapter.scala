c275c00ee2b64597250366eb465a39144370e9b0
Alec Roelke <alec.roelke@gmail.com>
2021-12-18 10:16:14 -0500

    Comment ColorAdapter

--- src/main/scala/editor/util/ColorAdapter.scala
+++ src/main/scala/editor/util/ColorAdapter.scala
@@ -9,6 +9,11 @@ import java.lang.reflect.Type
 import com.google.gson.JsonElement
 import com.google.gson.JsonDeserializationContext
 
+/**
+ * Serializer for [[Color]]s that converts them between ARGB hex strings and [[Color]] instances for use with JSON.
+ * @constructor create a new [[Color]] serializer/deserializer
+ * @author Alec Roelke
+ */
 class ColorAdapter extends JsonSerializer[Color] with JsonDeserializer[Color] {
   override def serialize(src: Color, typeOfSrc: Type, context: JsonSerializationContext) = JsonPrimitive(src.getRGB.toHexString)
 
