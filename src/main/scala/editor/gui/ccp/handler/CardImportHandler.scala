762dd9b5356678ad5ba350d34b6d6e399e309b0e
Alec Roelke <alec.roelke@gmail.com>
2021-12-20 12:29:04 -0500

    Comment CardImportHandler and improve code

--- src/main/scala/editor/gui/ccp/handler/CardImportHandler.scala
+++ src/main/scala/editor/gui/ccp/handler/CardImportHandler.scala
@@ -1,22 +1,31 @@
 package editor.gui.ccp.handler
 
-import javax.swing.TransferHandler
-import editor.gui.editor.EditorFrame
-import editor.gui.ccp.data.DataFlavors
 import editor.database.card.Card
+import editor.gui.ccp.data.DataFlavors
+import editor.gui.editor.EditorFrame
+
 import java.awt.datatransfer.UnsupportedFlavorException
 import java.io.IOException
+import javax.swing.TransferHandler
 
+/**
+ * Import handler for [[Card]]s, which adds one copy of each to the deck.
+ * 
+ * @param editor frame containing the deck to modify
+ * @param id list to add cards to
+ * 
+ * @author Alec Roelke
+ */
 class CardImportHandler(editor: EditorFrame, id: Int) extends TransferHandler with ImportHandler {
   override def supportedFlavor = DataFlavors.cardFlavor
   override def canImport(supp: TransferHandler.TransferSupport) = supp.isDataFlavorSupported(supportedFlavor)
   override def importData(supp: TransferHandler.TransferSupport) = if (!canImport(supp)) false else {
     try {
       val data = supp.getTransferable.getTransferData(supportedFlavor) match {
-        case a: Array[?] => a.collect{ case e if e.isInstanceOf[Card] => e.asInstanceOf[Card] }
+        case a: Array[?] => a.toSeq.collect{ case c: Card => c }
         case x => throw ClassCastException(s"needed ${classOf[Card]}, got ${x.getClass}")
       }
-      editor.lists(id) ++= data.toSeq -> 1
+      editor.lists(id) ++= data -> 1
     } catch {
       case _: UnsupportedFlavorException => false
       case _: IOException => false
