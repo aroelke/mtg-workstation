078775e3c02deb9a6b0041e062bb9788649dfba0
Alec Roelke <alec.roelke@gmail.com>
2021-12-20 13:37:20 -0500

    Comment CategoryTransferHandler

--- src/main/scala/editor/gui/ccp/handler/CategoryTransferHandler.scala
+++ src/main/scala/editor/gui/ccp/handler/CategoryTransferHandler.scala
@@ -1,12 +1,24 @@
 package editor.gui.ccp.handler
 
 import editor.collection.deck.Category
-import java.awt.Component
-import javax.swing.TransferHandler
-import javax.swing.JComponent
 import editor.gui.ccp.data.CategoryTransferData
-import java.awt.datatransfer.Transferable
 
+import java.awt.Component
+import java.awt.datatransfer.Transferable
+import javax.swing.JComponent
+import javax.swing.TransferHandler
+
+/**
+ * Handler for transferring [[Category]]s beween elements.
+ * 
+ * @constructor create a new category transfer handler with functions indicating how to transfer components
+ * @param supplier function supplying the [[Category]] to transfer
+ * @param contains predicate indicating if a category is already contained by the target element
+ * @param add function specifying how to add the [[Category]] to the target element
+ * @param remove function specifying how to remove the [[Category]] from the source element if it is to be moved rather than copied
+ * 
+ * @author Alec Roelke
+ */
 class CategoryTransferHandler(supplier: () => Category, contains: (Category) => Boolean, add: (Category) => Boolean, remove: (Category) => Unit) extends EditorTransferHandler(CategoryImportHandler(contains, add)) {
   override def getSourceActions(c: JComponent) = TransferHandler.MOVE | TransferHandler.COPY
   override def createTransferable(c: JComponent) = CategoryTransferData(supplier())
