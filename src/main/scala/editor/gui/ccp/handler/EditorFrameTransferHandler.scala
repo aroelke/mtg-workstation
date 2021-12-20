0ae9815786dbfdaece3d96d752cf31c643e455f1
Alec Roelke <alec.roelke@gmail.com>
2021-12-20 14:04:49 -0500

    Convert EditorFrameTransferHandler to Scala

---
+++ src/main/scala/editor/gui/ccp/handler/EditorFrameTransferHandler.scala
@@ -0,0 +1,15 @@
+package editor.gui.ccp.handler
+
+import editor.gui.editor.EditorFrame
+
+/**
+ * Transfer handler for importing cards into decks via the [[EditorFrame]] itself, rather than the tables it contains. Mainly a convenience
+ * for drag-and-drop.
+ * 
+ * @constructor create a new editor frame transfer handler for a particular list in a particular [[EditorFrame]]
+ * @param editor frame to which the handler belongs
+ * @param id ID of the list
+ * 
+ * @author Alec Roelke
+ */
+class EditorFrameTransferHandler(editor: EditorFrame, id: Int) extends EditorTransferHandler(EntryImportHandler(editor, id), CardImportHandler(editor, id))
