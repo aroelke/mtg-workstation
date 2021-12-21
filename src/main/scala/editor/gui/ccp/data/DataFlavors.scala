b86748051970c1944eb12614b43809bf230bc0e2
Alec Roelke <alec.roelke@gmail.com>
2021-12-21 09:34:35 -0500

    Comment DataFlavors

--- src/main/scala/editor/gui/ccp/data/DataFlavors.scala
+++ src/main/scala/editor/gui/ccp/data/DataFlavors.scala
@@ -1,13 +1,21 @@
 package editor.gui.ccp.data
 
-import java.awt.datatransfer.DataFlavor
-import editor.database.card.Card
 import editor.collection.deck.Category
+import editor.database.card.Card
 
+import java.awt.datatransfer.DataFlavor
+
+/**
+ * Collection of data flavors used for copying data.
+ * @author Alec Roelke
+ */
 object DataFlavors {
+  /** Data flavor used for copying cards, typically from the inventory panel. */
   lazy val cardFlavor = DataFlavor(s"${DataFlavor.javaJVMLocalObjectMimeType};class=\"${classOf[Array[Card]].getName}\"", "Card Array")
 
+  /** Data flavor used for copying cards and metadata, typically from a deck table. */
   lazy val entryFlavor = DataFlavor(s"${DataFlavor.javaJVMLocalObjectMimeType};class=\"${classOf[EntryTransferData].getName}\"", "Deck Entries")
 
+  /** Data flavor used for copying categorizations and (TBD) cards in catgories. */
   lazy val categoryFlavor = DataFlavor(s"${DataFlavor.javaJVMLocalObjectMimeType};class=\"${classOf[Category].getName}\"", "Categorization")
 }
