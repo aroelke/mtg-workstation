7ec16c59378bcf93c2a7b064c617d3799e42a62d
Alec Roelke <alec.roelke@gmail.com>
2021-12-24 08:49:28 -0500

    Comment BinaryFilterPanel

--- src/main/scala/editor/gui/filter/editor/BinaryFilterPanel.scala
+++ src/main/scala/editor/gui/filter/editor/BinaryFilterPanel.scala
@@ -1,11 +1,20 @@
 package editor.gui.filter.editor
 
+import editor.database.attributes.CardAttribute
 import editor.filter.leaf.FilterLeaf
+
 import java.awt.GridLayout
 import javax.swing.BorderFactory
 import javax.swing.JLabel
-import editor.database.attributes.CardAttribute
 
+/**
+ * A filter "editor" panel that creates a filter that always returns true when testing a card or false.
+ * 
+ * @constructor create a new binary filter panel
+ * @param allow whether or not the filter should allow cards through or not
+ * 
+ * @author Alec Roelke
+ */
 class BinaryFilterPanel(allow: Boolean) extends FilterEditorPanel[FilterLeaf[?]] {
   val All = "This clause will match every card."
   val None = "This clause will not match any card."
