a4dee4b263ebdb1616d2928df846a622bd24648b
Alec Roelke <alec.roelke@gmail.com>
2021-12-24 13:31:43 -0500

    Comment ManaCostFilterPanel

--- src/main/scala/editor/gui/filter/editor/ManaCostFilterPanel.scala
+++ src/main/scala/editor/gui/filter/editor/ManaCostFilterPanel.scala
@@ -1,20 +1,32 @@
 package editor.gui.filter.editor
 
-import editor.filter.leaf.ManaCostFilter
-import javax.swing.BoxLayout
-import java.awt.Color
-import editor.gui.generic.ComboBoxPanel
-import editor.util.Containment
-import javax.swing.JTextField
-import editor.gui.generic.DocumentChangeListener
 import editor.database.attributes.ManaCost
-import scala.jdk.OptionConverters._
 import editor.filter.leaf.FilterLeaf
-import javax.swing.event.DocumentEvent
+import editor.filter.leaf.ManaCostFilter
+import editor.gui.generic.ComboBoxPanel
+import editor.gui.generic.DocumentChangeListener
+import editor.util.Containment
 
+import java.awt.Color
+import javax.swing.BoxLayout
+import javax.swing.JTextField
+import javax.swing.event.DocumentEvent
+import scala.jdk.OptionConverters._
+
+/**
+ * Convenience constructors for [[ManaCostFilterPanel]].
+ * @author Alec Roelke
+ */
 object ManaCostFilterPanel {
+  /** @return a new, empty [[ManaCostFilterPanel]] */
   def apply() = new ManaCostFilterPanel
 
+  /**
+   * Create a new [[ManaCostFilterPanel]] pre-populated with the contents of a filter.
+   * 
+   * @param filter filter to use to populate the new panel
+   * @return a new mana cost filter panel containing the string version of the filter's cost
+   */
   def apply(filter: ManaCostFilter) = {
     val panel = new ManaCostFilterPanel
     panel.setContents(filter)
@@ -22,6 +34,12 @@ object ManaCostFilterPanel {
   }
 }
 
+/**
+ * A panel for customizing a [[ManaCostFilter]] by setting the symbols in it and how it should contain them.
+ * Symbols are set using text, with each of their strings surrounded by braces ({}).
+ * 
+ * @author Alec Roelke
+ */
 class ManaCostFilterPanel extends FilterEditorPanel[ManaCostFilter] {
   private val Valid = Color.WHITE
   private val Invalid = Color.PINK
