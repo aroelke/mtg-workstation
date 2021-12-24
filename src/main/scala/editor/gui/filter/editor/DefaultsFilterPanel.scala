9b5b4e2aa3168d688cbb7fc7b8c489847516c558
Alec Roelke <alec.roelke@gmail.com>
2021-12-24 09:45:05 -0500

    Comment DefaultsFilterPanel
    Also put its returned filter into a group

--- src/main/scala/editor/gui/filter/editor/DefaultsFilterPanel.scala
+++ src/main/scala/editor/gui/filter/editor/DefaultsFilterPanel.scala
@@ -1,12 +1,22 @@
 package editor.gui.filter.editor
 
-import editor.filter.leaf.FilterLeaf
-import java.awt.FlowLayout
 import editor.collection.deck.Category
-import editor.gui.settings.SettingsDialog
-import scala.collection.immutable.ListMap
+import editor.filter.FilterGroup
+import editor.filter.leaf.FilterLeaf
 import editor.gui.generic.ComboBoxPanel
+import editor.gui.settings.SettingsDialog
 
+import java.awt.FlowLayout
+import scala.collection.immutable.ListMap
+
+/**
+ * Panel that contains shortcuts for adding filters based on the filters of the preset categories. This panel
+ * doesn't actually correspond to a filter; internally, it is replaced by the contents of the filter of the
+ * selected preset category.  If the filter is viewed again, this panel will be replaced by a group containing
+ * the selected preset category's filter.
+ *
+ * @author Alec Roelke
+ */
 class DefaultsFilterPanel extends FilterEditorPanel[FilterLeaf[?]] {
   setLayout(FlowLayout(FlowLayout.LEFT, 0, 0))
 
@@ -14,6 +24,6 @@ class DefaultsFilterPanel extends FilterEditorPanel[FilterLeaf[?]] {
   private val defaults = ComboBoxPanel(categories.keys.toArray)
   add(defaults)
 
-  override def filter = Category(categories(defaults.getSelectedItem)).getFilter
+  override def filter = FilterGroup(Category(categories(defaults.getSelectedItem)).getFilter)
   override def setContents(filter: FilterLeaf[?]) = {}
 }
