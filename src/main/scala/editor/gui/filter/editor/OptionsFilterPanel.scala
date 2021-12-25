7c4c25631841867367b74e77e5c052e7926cb484
Alec Roelke <alec.roelke@gmail.com>
2021-12-24 22:19:59 -0500

    Comment OptionsFilterPanel

--- src/main/scala/editor/gui/filter/editor/OptionsFilterPanel.scala
+++ src/main/scala/editor/gui/filter/editor/OptionsFilterPanel.scala
@@ -1,35 +1,55 @@
 package editor.gui.filter.editor
 
-import editor.filter.leaf.options.OptionsFilter
 import editor.database.attributes.CardAttribute
-import java.awt.BorderLayout
-import editor.gui.generic.ComboBoxPanel
-import editor.util.Containment
-import editor.gui.generic.ScrollablePanel
-import javax.swing.BoxLayout
-import editor.gui.generic.ButtonScrollPane
-import javax.swing.JComboBox
-import javax.swing.BorderFactory
-import javax.swing.JPanel
-import editor.util.PopupMenuListenerFactory
-import javax.swing.plaf.basic.BasicComboPopup
-import javax.swing.SwingUtilities
-import javax.swing.JScrollPane
-import java.awt.Dimension
-import java.awt.GridLayout
-import javax.swing.JLabel
-import javax.swing.SwingConstants
-import java.awt.Font
-import java.awt.Component
-import editor.util.MouseListenerFactory
-import editor.util.UnicodeSymbols
-import java.awt.Color
-import scala.jdk.CollectionConverters._
 import editor.filter.leaf.FilterLeaf
+import editor.filter.leaf.options.OptionsFilter
+import editor.gui.generic.ButtonScrollPane
+import editor.gui.generic.ComboBoxPanel
+import editor.gui.generic.ScrollablePanel
+import editor.util.Containment
+import editor.util.MouseListenerFactory
+import editor.util.PopupMenuListenerFactory
+import editor.util.UnicodeSymbols
 
+import java.awt.BorderLayout
+import java.awt.Color
+import java.awt.Component
+import java.awt.Dimension
+import java.awt.Font
+import java.awt.GridLayout
+import javax.swing.BorderFactory
+import javax.swing.BoxLayout
+import javax.swing.JComboBox
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.JScrollPane
+import javax.swing.SwingConstants
+import javax.swing.SwingUtilities
+import javax.swing.plaf.basic.BasicComboPopup
+import scala.jdk.CollectionConverters._
+
+/**
+ * Convenience constructors for [[OptionsFilterPanel]].
+ * @author Alec Roelke
+ */
 object OptionsFilterPanel {
+  /**
+   * Create a new [[OptionsFilterPanel]] that filters the given attribute using the given options.
+   * 
+   * @param attribute card attribute to filter
+   * @param options values of the attribute that can be chosen
+   * @return an [[OptionsFilterPanel]] for a filter of the given attribute with the given possible values
+   */
   def apply[T <: AnyRef](attribute: CardAttribute, options: Array[T]) = new OptionsFilterPanel(attribute, options)
 
+  /**
+   * Create a new [[OptionsFilterPanel]] with the given filter and possible options to choose from. The attribute
+   * used for filtering is inferred form the filter.
+   * 
+   * @param filter filter used to infer the attribute and pre-populate the panel
+   * @param options values of the attribute that can be chosen
+   * @return an [[OptionsFilterPanel]] populated with the contents of the filter
+   */
   def apply[T <: AnyRef](filter: OptionsFilter[T], options: Array[T]) = {
     val panel = new OptionsFilterPanel(filter.`type`, options)
     panel.setContents(filter)
@@ -37,6 +57,19 @@ object OptionsFilterPanel {
   }
 }
 
+/**
+ * A filter editor panel that is used to customize a filter that selects one or more items from a number of
+ * predefined options. Each selected option is a combo box with all of the options and a pair of small buttons
+ * that allows for adding another selection or removing an existing one. If enough boxes are added, arrows on
+ * the left and right side will become enabled to allow for scrolling.
+ * 
+ * @constructor create a new options filter panel for filtering a particular attribute using a particular set of
+ * options
+ * @param attribute card attribute to filter for
+   @param options options that can be chosen for the filter value
+ * 
+ * @author Alec Roelke
+ */
 class OptionsFilterPanel[T <: AnyRef](attribute: CardAttribute, options: Array[T]) extends FilterEditorPanel[OptionsFilter[T]] {
   private val MaxComboWidth = 100
 
