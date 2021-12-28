87d56a49ed1b3c3c5fb80c10e61dd1a0aea24538
Alec Roelke <alec.roelke@gmail.com>
2021-12-28 15:34:41 -0500

    Convert FilterAdapter to Scala

---
+++ src/main/scala/editor/serialization/FilterAdapter.scala
@@ -0,0 +1,27 @@
+package editor.serialization
+
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonSerializationContext
+import com.google.gson.JsonSerializer
+import editor.database.attributes.CardAttribute
+import editor.filter.Filter
+import editor.filter.FilterGroup
+
+import java.lang.reflect.Type
+
+/**
+ * JSON serializer/deserializer for [[Filter]]s using their methods for converting to/from JSON objects.
+ * @author Alec Roelke
+ */
+class FilterAdapter extends JsonSerializer[Filter] with JsonDeserializer[Filter] {
+  override def deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext) = {
+    val attribute = CardAttribute.fromString(json.getAsJsonObject.get("type").getAsString)
+    val filter = if (attribute == CardAttribute.GROUP) FilterGroup() else CardAttribute.createFilter(attribute)
+    filter.fromJsonObject(json.getAsJsonObject)
+    filter
+  }
+
+  override def serialize(src: Filter, typeOfSrc: Type, context: JsonSerializationContext) = src.toJsonObject
+}
