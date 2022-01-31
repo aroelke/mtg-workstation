839835985dc063b41cfad0c31c703ac0ecb6bda4
Alec Roelke <alec.roelke@gmail.com>
2022-01-30 21:54:28 -0500

    Comment VariableSymbol

--- src/main/scala/editor/database/symbol/VariableSymbol.scala
+++ src/main/scala/editor/database/symbol/VariableSymbol.scala
@@ -5,9 +5,22 @@ import editor.database.attributes.ManaType
 import scala.jdk.CollectionConverters._
 import scala.jdk.OptionConverters._
 
+/**
+ * Companion to [[VariableSymbol]] containing all the possible variables used by the game and functions for parsing
+ * them from strings.
+ *
+ * @author Alec Roelke
+ */
 object VariableSymbol {
+  /** All the possible [[VariableSymbols]]: X, Y, and Z. */
   val values = Seq('X', 'Y', 'Z').map((v) => v.toString -> VariableSymbol(v)).toMap
 
+  /**
+   * Parse a [[VariableSymbol]] from a string.
+   * 
+   * @param s string to parse
+   * @return a [[VariableSymbol]] using the letter in the string as its variable, or None if there isn't one
+   */
   def parse(s: String) = values.get(s)
 
   @deprecated val SYMBOLS = values.asJava
@@ -15,6 +28,14 @@ object VariableSymbol {
   @deprecated def tryParseVariableSymbol(s: String) = parse(s).toJava
 }
 
+/**
+ * A symbol that can be paid using any amount of mana, indicating using a letter like X.
+ * 
+ * @constructor create a new variable symbol using a particular variable name
+ * @param variable letter to use as the variable name
+ * 
+ * @author Alec Roelke
+ */
 class VariableSymbol private(private val variable: Char) extends ManaSymbol(s"${variable.toLower}_mana.png", variable.toString.toUpperCase, 0) {
   override def colorIntensity = ManaSymbol.createIntensity(ColorIntensity(ManaType.COLORLESS, 0.5))
 
