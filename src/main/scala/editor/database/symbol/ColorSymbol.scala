c6b3059a21bd97f49fe0e4b87cde4bcf6dbf7264
Alec Roelke <alec.roelke@gmail.com>
2022-01-31 22:34:53 -0500

    Comment ColorSymbol

--- src/main/scala/editor/database/symbol/ColorSymbol.scala
+++ src/main/scala/editor/database/symbol/ColorSymbol.scala
@@ -5,9 +5,21 @@ import editor.database.attributes.ManaType
 import scala.jdk.CollectionConverters._
 import scala.jdk.OptionConverters._
 
+/**
+ * Companion to [[ColorSymbol]] containing all (currently) possible mana type symbols, as well as functions for
+ * parsing them from strings.
+ * @author Alec Roelke
+ */
 object ColorSymbol {
+  /** All possible mana type symbols. @see [[ManaType]] */
   val values = ManaType.values.map(s => s -> ColorSymbol(s)).toMap
 
+  /**
+   * Parse a [[ColorSymbol]] from a string.
+   * 
+   * @param s string to parse
+   * @return the [[ColorSymbol]] corresponding to the string, or None if there isn't one
+   */
   def parse(s: String) = Option(ManaType.tryParseManaType(s)).map(values)
 
   @deprecated val SYMBOLS = values.asJava
@@ -15,6 +27,14 @@ object ColorSymbol {
   @deprecated def parseColorSymbol(s: String) = values(ManaType.parseManaType(s))
 }
 
+/**
+ * Mana symbol representing a specific type of mana.
+ * 
+ * @constructor create a new symbol for a mana type
+ * @param color type of mana the symbol represents
+ * 
+ * @author Alec Roelke
+ */
 class ColorSymbol private(private val color: ManaType) extends ManaSymbol(s"${color.toString.toLowerCase}_mana.png", color.shorthand.toString, 1) {
   override def colorIntensity = ManaSymbol.createIntensity(ColorIntensity(color, 1))
 
