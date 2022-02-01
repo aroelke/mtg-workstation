290812b050251baf806f97e5abeb58df9cbd875d
Alec Roelke <alec.roelke@gmail.com>
2022-01-31 22:07:21 -0500

    Comment GenericSymbol

--- src/main/scala/editor/database/symbol/GenericSymbol.scala
+++ src/main/scala/editor/database/symbol/GenericSymbol.scala
@@ -2,17 +2,31 @@ package editor.database.symbol
 
 import scala.jdk.OptionConverters._
 
+/**
+ * Companion to [[GenericSymbol]] containing all the currently-used generic mana symbols and functions for parsing them
+ * from strings.
+ * @author Alec Roelke
+ */
 object GenericSymbol {
+  /** Highest currently-used contiguous generic mana symbol. */
   val Max = 20
 
+  /** All of the contiguous generic mana symbols from {0} through {[[Max]]}. */
   val Contiguous = (0 to Max).map(new GenericSymbol(_)).toSeq
 
+  /** Generic mana symbol for 100 mana. */
   val Hundred = new GenericSymbol(100)
 
+  /** Generic mana symbol for 1000000 mana. */
   val Million = new GenericSymbol(1000000)
 
+  /** All currently-used generic mana symbols: {0} through {[[Max]]}, {100}, and {1000000}. */
   val values = (Contiguous.zipWithIndex.map{ case (a, b) => a -> b } ++ Seq(100 -> Hundred, 1000000 -> Million)).toMap
 
+  /**
+   * @param n amount of mana needed to pay for the symbol
+   * @return the [[GenericSymbol]] corresponding to the amount of mana needed to pay it
+   */
   def apply(n: Int) = n match {
     case 1000000 => Million
     case 100 => Hundred
@@ -20,6 +34,12 @@ object GenericSymbol {
     case _ => throw ArrayIndexOutOfBoundsException(n)
   }
 
+  /**
+   * Parse a [[GenericSymbol]] from a string, which should contain only the amount of mana it represents.
+   * 
+   * @param s string to parse
+   * @return the [[GenericSymbol]] represented by the string, or None if there isn't one
+   */
   def parse(s: String) = try {
     Some(apply(s.toInt))
   } catch case _ @ (_: NumberFormatException | _: ArrayIndexOutOfBoundsException) => None
@@ -33,6 +53,14 @@ object GenericSymbol {
   @deprecated def parseGenericSymbol(s: String) = apply(s.toInt)
 }
 
+/**
+ * A mana symbol that can be paid for using an amount of any type of mana in any combination.
+ * 
+ * @constructor create a new generic mana symbol for a particular amount of mana
+ * @param amount amount of mana needed to pay for the symbol
+ * 
+ * @author Alec Roelke
+ */
 class GenericSymbol private(amount: Int) extends ManaSymbol(s"${amount}_mana.png", amount.toString, amount) {
   override def colorIntensity = ManaSymbol.createIntensity()
 
