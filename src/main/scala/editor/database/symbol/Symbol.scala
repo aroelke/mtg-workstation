1afcc3c397732b2f13407965009882a4a757229b
Alec Roelke <alec.roelke@gmail.com>
2022-02-04 09:50:01 -0500

    Comment Symbol

--- src/main/scala/editor/database/symbol/Symbol.scala
+++ src/main/scala/editor/database/symbol/Symbol.scala
@@ -1,18 +1,29 @@
 package editor.database.symbol
 
-import javax.swing.ImageIcon
-import javax.imageio.ImageIO
-import java.io.IOException
 import java.awt.Image
+import java.io.IOException
 import java.util.regex.Pattern
-
+import javax.imageio.ImageIO
+import javax.swing.ImageIcon
 import scala.jdk.OptionConverters._
 
+/**
+ * Companion to [[Symbol]] containing global data and methods for parsing from strings.
+ * @author Alec Roelke
+ */
 object Symbol {
+  /** Regular expression for retrieving a [[Symbol]]'s text from its string representation. */
   val Regex = Pattern.compile(raw"\{([^}]+)\}")
 
+  /** Icon name to use for [[Symbol]]s whose icons are unknown. */
   val Unknown = "unknown.png"
 
+  /**
+   * Parse a [[Symbol]] from a string. The string should be its text representation without braces.
+   * 
+   * @param s string to parse
+   * @return the [[Symbol]] represented by the string, or None if there isn't one
+   */
   def parse(s: String) = {
     ManaSymbol.parse(s).orElse(
     FunctionalSymbol.parse(s)
@@ -25,6 +36,16 @@ object Symbol {
   @deprecated def parseSymbol(s: String) = parse(s).getOrElse(throw IllegalArgumentException(s"$s is not a symbol"))
 }
 
+/**
+ * A symbol that can appear on a card. Since there is a finite set of symbols, individual ones cannot be instantiated;
+ * rather, they are accessed via static data structure or by parsing from string.
+ * 
+ * @constructor create a new symbol
+ * @param name file name containing the icon to show for the symbol
+ * @param text text representation of the symbol; when printed out, this will be displayed between braces
+ * 
+ * @author Alec Roelke
+ */
 abstract class Symbol(val name: String, private val text: String) {
   val icon = try {
     ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/icons/$name")))
