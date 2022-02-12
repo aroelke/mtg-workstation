96b2c0f039bae0786946e28297aa1a3ac975da4b
Alec Roelke <alec.roelke@gmail.com>
2022-02-11 19:03:50 -0500

    Convert ModalCard to Scala

---
+++ src/main/scala/editor/database/card/ModalCard.scala
@@ -0,0 +1,20 @@
+package editor.database.card
+
+import CardLayout._
+
+/**
+ * A [[Card]] with two faces, either of which can be played, and doesn't typically switch until it
+ * leaves the battlefield or stack. Its mana value is that of its front face.
+ * 
+ * @constructor create a new modal double-faced card
+ * @param front front face of the card
+ * @param back back face of the card
+ * @author Alec Roelke
+ */
+@throws[IllegalArgumentException]("if both faces aren't modal double-faced cards")
+class ModalCard(front: Card, back: Card) extends MultiCard(MODAL_DFC, front, back) {
+  if (front.layout != MODAL_DFC || back.layout != MODAL_DFC)
+    IllegalArgumentException("can't join non-modal-double-faced cards into modal double-faced cards")
+
+  override lazy val manaValue = front.manaValue
+}
