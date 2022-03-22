bd79915f61ff63e1d47ebe08a45e0ba5dea2c4bc
Alec Roelke <alec.roelke@gmail.com>
2022-03-22 10:42:11 -0400

    Fix Card serialization

--- src/main/scala/editor/serialization/CardAdapter.scala
+++ src/main/scala/editor/serialization/CardAdapter.scala
@@ -35,7 +35,7 @@ class CardAdapter extends JsonSerializer[Card] with JsonDeserializer[Card] {
 
   override def serialize(src: Card, typeOfSrc: Type, context: JsonSerializationContext) = {
     val card = JsonObject()
-    card.addProperty("scryfallid", src.scryfallid(0))
+    card.addProperty("scryfallid", src(0).scryfallid)
     card.addProperty("name", src.name)
     card.addProperty("expansion", src.expansion.name)
     card
