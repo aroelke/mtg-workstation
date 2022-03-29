e32937550a302b30566789588d034c7f958ca900
Alec Roelke <alec.roelke@gmail.com>
2022-03-28 21:16:37 -0400

    Comment TypeLine

--- src/main/scala/editor/database/attributes/TypeLine.scala
+++ src/main/scala/editor/database/attributes/TypeLine.scala
@@ -1,20 +1,51 @@
 package editor.database.attributes
 
-import scala.collection.immutable.ListSet
-import scala.collection.AbstractIterable
 import editor.util.UnicodeSymbols
 
+import scala.collection.AbstractIterable
+import scala.collection.immutable.ListSet
+
+/**
+ * The set of all types (card types, supertypes, and subtypes) on a face of a card. Use [[toString]]
+ * to convert to the canonical type line as it appears on a card's Oracle text.
+ * 
+ * @constructor create a new type line
+ * @param types card types of the card face; must have at least one item
+ * @param subtypes subtypes of the card face, if any
+ * @param supertypes supertypes of the card face, if any
+ * 
+ * @author Alec Roelke
+ */
 final case class TypeLine(types: ListSet[String], subtypes: ListSet[String] = ListSet.empty, supertypes: ListSet[String] = ListSet.empty) extends AbstractIterable[String] with Ordered[TypeLine] {
   if (types.isEmpty)
     throw IllegalArgumentException("a card must have at least one type")
 
+  private lazy val ordered = supertypes ++ types ++ subtypes
+
+  /**
+   * @param s type to test for; is case sensitive
+   * @return true if the type line contains the given type, including case, and false otherwise
+   */
   def contains(s: String) = types.contains(s) || subtypes.contains(s) || supertypes.contains(s)
   
-  def concat(tl: TypeLine) = TypeLine(types ++ tl.types, subtypes ++ tl.subtypes, supertypes ++ tl.supertypes)
+  /**
+   * @param s type to test for, ignoring case
+   * @return true if the type line contains the given type, ignoring case, and false otherwise
+   */
+  def containsIgnoreCase(s: String) = ordered.exists(_.equalsIgnoreCase(s))
 
-  def ++(tl: TypeLine) = concat(tl)
+  /**
+   * Create a new type line containing all the types of this one followed by the types of another.
+   * 
+   * @param that type line to add
+   * @return a new type line with all the types of this one and that one
+   */
+  def concat(that: TypeLine) = TypeLine(types ++ that.types, subtypes ++ that.subtypes, supertypes ++ that.supertypes)
 
-  override def iterator = (supertypes ++ types ++ subtypes).iterator
+  /** Alias for [[concat]]. */
+  def ++(that: TypeLine) = concat(that)
+
+  override def iterator = ordered.iterator
 
   override def compare(that: TypeLine) = {
     if (types != that.types)
