2b067e2e9c0999e6db49ea413608490791b4815c
Alec Roelke <alec.roelke@gmail.com>
2022-04-08 14:38:20 -0400

    Convert BlockFilter to Scala

---
+++ src/main/scala/editor/filter/leaf/options/single/BlockFilter.scala
@@ -0,0 +1,24 @@
+package editor.filter.leaf.options.single
+
+import com.google.gson.JsonElement
+import com.google.gson.JsonPrimitive
+import editor.database.attributes.CardAttribute
+
+/**
+ * A filter that groups cards by the block they belong in.
+ * @author Alec Roelke
+ */
+class BlockFilter extends SingletonOptionsFilter[String](CardAttribute.BLOCK, _.expansion.block) {
+  override protected def copyLeaf = {
+    val filter = CardAttribute.createFilter(CardAttribute.BLOCK).asInstanceOf[BlockFilter]
+    filter.contain = contain
+    filter.selected = java.util.HashSet(selected)
+    filter
+  }
+
+  override protected def convertFromString(str: String) = str
+
+  override protected def convertToJson(item: String) = JsonPrimitive(item)
+
+  override protected def convertFromJson(item: JsonElement) = item.getAsString
+}
