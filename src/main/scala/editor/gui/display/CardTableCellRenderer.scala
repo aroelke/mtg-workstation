97d3542547fb5dbaadd4db90e85156f5b3c74262
Alec Roelke <alec.roelke@gmail.com>
2022-04-07 21:03:13 -0400

    Fix text color in selected table rows

--- src/main/scala/editor/gui/display/CardTableCellRenderer.scala
+++ src/main/scala/editor/gui/display/CardTableCellRenderer.scala
@@ -26,6 +26,7 @@ import javax.swing.UIManager
 import javax.swing.border.Border
 import javax.swing.table.DefaultTableCellRenderer
 import scala.jdk.CollectionConverters._
+import java.awt.Panel
 
 private object CardTableCellRenderer {
   val cache = collection.mutable.Map[Seq[ManaCost], Seq[Seq[Icon]]]()
@@ -48,66 +49,72 @@ class CardTableCellRenderer extends DefaultTableCellRenderer {
     val c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column)
     table.getModel match {
       case m: CardTableModel =>
-        def finishPanel(p: JComponent, b: Border = BorderFactory.createEmptyBorder(1, 1, 1, 1)) = {
-          p.setBorder(if (hasFocus) UIManager.getBorder("Table.focusCellHighlightBorder") else b)
-          p.setForeground(c.getForeground)
-          p.setBackground(c.getBackground)
-          p
+        def setColors[C <: JComponent](component: C) = {
+          component.setForeground(c.getForeground)
+          component.setBackground(c.getBackground)
+          component
+        }
+        def tableLabel(contents: String | Icon) = setColors(contents match {
+          case text: String => JLabel(text)
+          case icon: Icon => JLabel(icon)
+        })
+        def tablePanel(panel: JPanel = JPanel(), border: Border = BorderFactory.createEmptyBorder(1, 1, 1, 1)) = {
+          panel.setBorder(if (hasFocus) UIManager.getBorder("Table.focusCellHighlightBorder") else border)
+          setColors(panel)
         }
         m.columns(column) match {
           case CardAttribute.MANA_COST =>
-            val panel = JPanel()
-            panel.setLayout(BoxLayout(panel, BoxLayout.X_AXIS))
             val costs = value match {
               case s: java.util.List[?] => s.asScala.toSeq.collect{ case cost: ManaCost => cost }
               case _ => Seq.empty
             }
             val icons = cache.getOrElseUpdate(costs, costs.map(_.asScala.toSeq.map(_.getIcon(13))))
+            val panel = tablePanel(border = BorderFactory.createEmptyBorder(0, 1, if (icons.length == 1) -1 else 0, 0))
+            panel.setLayout(BoxLayout(panel, BoxLayout.X_AXIS))
             for (i <- 0 until icons.size) {
               if (!icons(i).isEmpty) {
                 if (i > 0)
-                  panel.add(JLabel(Card.FaceSeparator))
-                icons(i).foreach{ icon => panel.add(JLabel(icon)) }
+                  panel.add(tableLabel(Card.FaceSeparator))
+                icons(i).foreach{ icon => panel.add(tableLabel(icon)) }
               }
             }
-            finishPanel(panel, BorderFactory.createEmptyBorder(0, 1, if (icons.length == 1) -1 else 0, 0))
+            panel
           case CardAttribute.MANA_VALUE | CardAttribute.MIN_VALUE | CardAttribute.MAX_VALUE =>
             val mv = Option(value) match {
               case Some(v: Double) => v
               case Some(v: Int) => v.toDouble
               case _ => 0.0
             }
-            val panel = JPanel(FlowLayout(FlowLayout.LEFT, 0, 0))
-            panel.add(JLabel(if (mv == mv.toInt) mv.toInt.toString else mv.toString))
-            finishPanel(panel)
+            val panel = tablePanel(JPanel(FlowLayout(FlowLayout.LEFT, 0, 0)))
+            panel.add(setColors(tableLabel(if (mv == mv.toInt) mv.toInt.toString else mv.toString)))
           case CardAttribute.COLORS | CardAttribute.COLOR_IDENTITY =>
-            val panel = JPanel()
+            val panel = tablePanel()
             panel.setLayout(BoxLayout(panel, BoxLayout.X_AXIS))
             value match {
-              case s: java.util.List[?] => s.asScala.toSeq.foreach{ case t: ManaType => panel.add(JLabel(ColorSymbol(t).scaled(13))) }
+              case s: java.util.List[?] => s.asScala.toSeq.foreach{ case t: ManaType => panel.add(tableLabel(ColorSymbol(t).scaled(13))) }
               case _ =>
             }
-            finishPanel(panel)
+            panel
           case CardAttribute.TYPE_LINE =>
-            val panel = JPanel(FlowLayout(FlowLayout.LEFT, 0, 0))
+            val panel = tablePanel(JPanel(FlowLayout(FlowLayout.LEFT, 0, 0)))
             value match {
-              case s: java.util.List[?] => panel.add(JLabel(s.asScala.toSeq.mkString(Card.FaceSeparator)))
+              case s: java.util.List[?] => panel.add(setColors(tableLabel(s.asScala.mkString(Card.FaceSeparator))))
               case _ => ""
             }
-            finishPanel(panel)
+            panel
           case CardAttribute.POWER | CardAttribute.TOUGHNESS | CardAttribute.LOYALTY =>
-            val panel = JPanel(FlowLayout(FlowLayout.LEFT, 0, 0))
+            val panel = tablePanel(JPanel(FlowLayout(FlowLayout.LEFT, 0, 0)))
             value match {
-              case s: java.util.List[?] => panel.add(JLabel(s.asScala.mkString(Card.FaceSeparator)))
+              case s: java.util.List[?] => panel.add(setColors(tableLabel(s.asScala.mkString(Card.FaceSeparator))))
               case _ =>
             }
-            finishPanel(panel)
+            panel
           case CardAttribute.CATEGORIES =>
             val categories = value match {
               case s: java.util.Set[?] => s.asScala.toSeq.collect{ case category: Category => category }.sortBy(_.getName)
               case _ => Seq.empty
             }
-            val panel = new JPanel {
+            val panel = tablePanel(new JPanel {
               override def paintComponent(g: Graphics) = {
                 super.paintComponent(g)
                 for (i <- 0 until categories.size) {
@@ -119,7 +126,7 @@ class CardTableCellRenderer extends DefaultTableCellRenderer {
                   g.drawRect(x, y, getHeight - 3, getHeight - 3)
                 }
               }
-            }
+            })
             if (!categories.isEmpty) {
               panel.setToolTipText(categories.map(_.getName).mkString(
                 s"<html>Categories:<br>${UnicodeSymbols.BULLET} ",
@@ -127,14 +134,14 @@ class CardTableCellRenderer extends DefaultTableCellRenderer {
                 "</html>"
               ))
             }
-            finishPanel(panel)
+            panel
           case CardAttribute.DATE_ADDED =>
-            val panel = JPanel(FlowLayout(FlowLayout.LEFT, 0, 0))
+            val panel = tablePanel(JPanel(FlowLayout(FlowLayout.LEFT, 0, 0)))
             value match {
-              case d: LocalDate => panel.add(JLabel(Deck.DATE_FORMATTER.format(d)))
+              case d: LocalDate => panel.add(setColors(tableLabel(Deck.DATE_FORMATTER.format(d))))
               case _ =>
             }
-            finishPanel(panel)
+            panel
           case _ => c
         }
       case _ => c
