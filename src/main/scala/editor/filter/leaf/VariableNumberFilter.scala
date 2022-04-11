c9e82cc10339473ea5a6321f483a3398f6a4d656
Alec Roelke <alec.roelke@gmail.com>
2022-04-11 19:13:58 -0400

    Fix VariableNumberFilter comments

--- src/main/scala/editor/filter/leaf/VariableNumberFilter.scala
+++ src/main/scala/editor/filter/leaf/VariableNumberFilter.scala
@@ -18,6 +18,7 @@ import scala.jdk.CollectionConverters._
  * @author Alec Roelke
  */
 class VariableNumberFilter(t: CardAttribute, value: (Card) => Iterable[Double], variable: (Card) => Boolean) extends NumberFilter(t, value(_).map(java.lang.Double(_)).toSeq.asJava) {
+  /** Whether or not the value of the attribute should be variable during a game. */
   var varies = false
 
   override protected def testFace(c: Card) = if (varies) variable(c) else super.testFace(c)
