81fca94a8cf3686fbef68d1529159212ffb33359
Alec Roelke <alec.roelke@gmail.com>
2022-05-01 09:26:37 -0400

    Convert CardListFormat to Scala

---
+++ src/main/scala/editor/collection/export/CardListFormat.scala
@@ -0,0 +1,35 @@
+package editor.collection.`export`
+
+import editor.collection.CardList
+import editor.gui.editor.DeckSerializer
+
+import java.io.InputStream
+
+/**
+ * Generic formatter for converting a [[CardList]] into a string or parsing a file into a deck.
+ * @author Alec Roelke
+ */
+trait CardListFormat {
+  /**
+   * Convert a [[CardList]] into a string. Which cards, and which attributes of and other information about the cards,
+   * that are included and how that information is arranged is implementation-dependent.
+   * 
+   * @param list list to convert
+   * @return the formatted string representing the card list
+   */
+  def format(list: CardList): String
+
+  /** The header of the string version of the list, or the empty string if there isn't one. */
+  def header: String
+
+  /** @return true if there is a header, and false otherwise */
+  def hasHeader = !header.isEmpty
+
+  /**
+   * Parse a stream into a new deck. The format of the file is implementation-dependent.
+   * 
+   * @param source stream to convert to a deck
+   * @return a [[DeckSerializer]] containing all of the information about the deck
+   */
+  def parse(source: InputStream): DeckSerializer
+}
