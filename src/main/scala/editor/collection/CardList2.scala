8b5542fb13bfb93f2f8750cc2cd10dcccbcbf2be
Alec Roelke <alec.roelke@gmail.com>
2022-05-21 22:07:02 -0400

    Add implicit conversion (Card, Int) => entry

--- src/main/scala/editor/collection/CardList2.scala
+++ src/main/scala/editor/collection/CardList2.scala
@@ -8,6 +8,7 @@ case class StandaloneEntry(card: Card, count: Int, dateAdded: LocalDate) extends
 }
 
 given card2Entry: Conversion[Card, CardListEntry] = StandaloneEntry(_, 1, LocalDate.now)
+given Conversion[(Card, Int), CardListEntry] with { def apply(card: (Card, Int)) = StandaloneEntry(card._1, card._2, LocalDate.now) }
 
 trait CardList2 extends collection.IndexedSeq[CardListEntry] {
   def contains(card: Card) = exists(_.card == card)
