75cecb6de38efb372a2f86b2d186a3c7bfc20b1a
Alec Roelke <alec.roelke@gmail.com>
2022-08-01 15:58:38 -0400

    Clean up FormatConstraints

--- src/main/scala/editor/database/FormatConstraints.scala
+++ src/main/scala/editor/database/FormatConstraints.scala
@@ -15,14 +15,12 @@ import javax.xml.crypto.Data
  * @author Alec Roelke
  */
 case class FormatConstraints(deckSize: Int = 60, isExact: Boolean = false, maxCopies: Int = 4, sideboardSize: Int = 15, hasCommander: Boolean = false) {
-  def this() = this(60, false, 4, 15, false)
-
   /**
    * @param name name of the format
    * @return An array containig the elements of this set of deckbuilding constraints
    * with the name of the format prepended, in the order specified by {@link #DATA_NAMES}.
    */
-  def toArray(name: String): Array[Any] = Array(name, deckSize, isExact, maxCopies, sideboardSize, hasCommander)
+  def toArray(name: String) = Array(name, deckSize, isExact, maxCopies, sideboardSize, hasCommander)
 }
 
 object FormatConstraints {
@@ -42,17 +40,13 @@ object FormatConstraints {
     "standard" -> FormatConstraints(),
     "vintage" -> FormatConstraints()
   )
-  @deprecated def CONSTRAINTS() = Constraints
 
   /** List of supported format names, in alphabetical order. */
   val FormatNames = Constraints.map{ case (name, _) => name }.toIndexedSeq.sorted
-  @deprecated def FORMAT_NAMES() = FormatNames
 
   /** List of types of each of the deckbuilding constraints. */
   val Classes = IndexedSeq(classOf[String], classOf[Integer], classOf[java.lang.Boolean], classOf[Integer], classOf[Integer], classOf[java.lang.Boolean])
-  @deprecated def CLASSES() = Classes
 
   /** The name of each type of deckbuilding constraint. */
   val DataNames = IndexedSeq("Name", "Deck Size", "Exact?", "Max Card Count", "Sideboard size", "Has Commander?")
-  @deprecated def DATA_NAMES() = DataNames
 }
