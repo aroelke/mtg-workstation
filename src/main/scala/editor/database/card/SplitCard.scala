af60af2868e98a6db32a3ff3b135641f15ffa740
Alec Roelke <alec.roelke@gmail.com>
2022-08-13 17:12:53 -0400

    Fix mana value of adventure cards

--- src/main/scala/editor/database/card/SplitCard.scala
+++ src/main/scala/editor/database/card/SplitCard.scala
@@ -14,10 +14,14 @@ import CardLayout.{ADVENTURE, AFTERMATH, SPLIT}
 class SplitCard(faces: Seq[Card]) extends MultiCard(faces(0).layout, faces.toIndexedSeq) {
   if (!faces.forall((f) => Seq(SPLIT, AFTERMATH, ADVENTURE).contains(f.layout)))
     throw IllegalArgumentException("can't create split cards out of non-split cards")
-  if (!faces.forall((f) => f.layout == faces(0).layout))
+  if (!faces.forall(_.layout == faces(0).layout))
     throw IllegalArgumentException("all faces of a split card must be of the same type")
   
-  override lazy val manaValue = faces.map(_.manaValue).sum
+  override lazy val manaValue = layout match {
+    case SPLIT | AFTERMATH => faces.map(_.manaValue).sum
+    case ADVENTURE => faces(0).manaValue
+    case _ => throw IllegalStateException(s"$layout isn't a split layout")
+  }
   override def imageNames = faces(0).imageNames
   override def multiverseid = faces(0).multiverseid
   override def scryfallid = faces(0).scryfallid
