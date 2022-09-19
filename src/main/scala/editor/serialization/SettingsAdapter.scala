8bd0e343ff49d264d8dc97f97fa62936463d41bf
Alec Roelke <alec.roelke@gmail.com>
2022-09-19 15:15:48 -0400

    Auto serialize EditorSettings

--- src/main/scala/editor/serialization/SettingsAdapter.scala
+++ src/main/scala/editor/serialization/SettingsAdapter.scala
@@ -32,17 +32,7 @@ class SettingsAdapter extends CustomSerializer[Settings](implicit formats => (
     ) },
   { case settings: Settings => JObject(List(
     JField("inventory", Extraction.decompose(settings.inventory)),
-    JField("editor", JObject(List(
-      JField("recents", Extraction.decompose(settings.editor.recents)),
-      JField("categories", Extraction.decompose(settings.editor.categories)),
-      JField("columns", JArray(settings.editor.columns.map(Extraction.decompose).toList)),
-      JField("stripe", Extraction.decompose(settings.editor.stripe)),
-      JField("hand", Extraction.decompose(settings.editor.hand)),
-      JField("legality", Extraction.decompose(settings.editor.legality)),
-      JField("manaValue", JString(settings.editor.manaValue)),
-      JField("backFaceLands", JArray(settings.editor.backFaceLands.map((l) => JString(l.toString)).toList)),
-      JField("manaAnalysis", Extraction.decompose(settings.editor.manaAnalysis))
-    ))),
+    JField("editor", Extraction.decompose(settings.editor)),
     JField("cwd", JString(settings.cwd))
   )) }
 ))
