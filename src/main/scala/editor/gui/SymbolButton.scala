c4a1300b91a3aa6ff2f29f81d101593dedec310f
aroelke <alec.roelke@gmail.com>
2023-02-24 22:43:14 -0500

    Add initial SymbolButton
    Right now it only supports ColorSymbols, but it should be
    extended to add any symbol

---
+++ src/main/scala/editor/gui/SymbolButton.scala
@@ -0,0 +1,35 @@
+package editor.gui
+
+import editor.database.symbol.ColorSymbol
+import editor.database.symbol.Symbol
+import javax.swing.JCheckBox
+import javax.swing.ImageIcon
+import javax.imageio.ImageIO
+import java.io.IOException
+import java.awt.Image
+import editor.gui.generic.ComponentUtils
+
+object SymbolButton {
+  private val IconHeight = 13
+
+  def apply(symbol: ColorSymbol, selected: Boolean = false) = try {
+    val str = symbol.color.toString.toLowerCase
+    val box = JCheckBox()
+
+    val unselected = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/$str/unselected.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
+    val disabledUnselected = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/$str/disabled_unselected.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
+    val disabledSelected = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/$str/disabled_selected.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
+    val pressed = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/$str/pressed.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
+
+    box.setIcon(unselected)
+    box.setSelectedIcon(symbol.scaled(IconHeight))
+    box.setDisabledIcon(disabledUnselected)
+    box.setDisabledSelectedIcon(disabledSelected)
+    box.setPressedIcon(pressed)
+    box.setRolloverIcon(unselected)
+    box.setRolloverSelectedIcon(symbol.scaled(IconHeight))
+
+    box.setSelected(selected)
+    box
+  } catch case x: IOException => JCheckBox()
+}
