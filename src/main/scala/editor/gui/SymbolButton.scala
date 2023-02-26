a91ddddae25d8f966b634cf33435640d347d892c
aroelke <alec.roelke@gmail.com>
2023-02-26 11:56:09 -0500

    Give Symbol an accessible name

--- src/main/scala/editor/gui/SymbolButton.scala
+++ src/main/scala/editor/gui/SymbolButton.scala
@@ -13,13 +13,12 @@ object SymbolButton {
   private val IconHeight = 13
 
   def apply(symbol: ColorSymbol, selected: Boolean = false) = try {
-    val str = symbol.color.toString.toLowerCase
     val box = JCheckBox()
 
-    val unselected = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/$str/unselected.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
-    val disabledUnselected = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/$str/disabled_unselected.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
-    val disabledSelected = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/$str/disabled_selected.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
-    val pressed = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/$str/pressed.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
+    val unselected = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/${symbol.name}/unselected.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
+    val disabledUnselected = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/${symbol.name}/disabled_unselected.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
+    val disabledSelected = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/${symbol.name}/disabled_selected.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
+    val pressed = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/${symbol.name}/pressed.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
 
     box.setIcon(unselected)
     box.setSelectedIcon(symbol.scaled(IconHeight))
