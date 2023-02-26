4cfe3ac6951b4454c3c50e1badd80091dc1f993e
aroelke <alec.roelke@gmail.com>
2023-02-26 12:45:14 -0500

    Update icons

--- src/main/scala/editor/gui/SymbolButton.scala
+++ src/main/scala/editor/gui/SymbolButton.scala
@@ -19,14 +19,16 @@ object SymbolButton {
     val disabledUnselected = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/${symbol.name}/disabled_unselected.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
     val disabledSelected = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/${symbol.name}/disabled_selected.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
     val pressed = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/${symbol.name}/pressed.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
+    val rolloverUnselected = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/${symbol.name}/rollover_unselected.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
+    val rolloverSelected = ImageIcon(ImageIO.read(getClass.getResourceAsStream(s"/images/ui/SymbolButton/${symbol.name}/rollover_selected.png")).getScaledInstance(-1, IconHeight, Image.SCALE_SMOOTH))
 
     box.setIcon(unselected)
     box.setSelectedIcon(symbol.scaled(IconHeight))
     box.setDisabledIcon(disabledUnselected)
     box.setDisabledSelectedIcon(disabledSelected)
     box.setPressedIcon(pressed)
-    box.setRolloverIcon(unselected)
-    box.setRolloverSelectedIcon(symbol.scaled(IconHeight))
+    box.setRolloverIcon(rolloverUnselected)
+    box.setRolloverSelectedIcon(rolloverSelected)
 
     box.setSelected(selected)
     box
