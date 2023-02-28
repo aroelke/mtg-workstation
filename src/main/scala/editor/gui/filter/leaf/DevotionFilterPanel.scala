da2672a36b570a4d187df8df9e3c8ce1d77e1eaf
Alec Roelke <alec.roelke@gmail.com>
2023-02-28 17:22:24 -0500

    Add convenience constructors for DevotionFilter

--- src/main/scala/editor/gui/filter/leaf/DevotionFilterPanel.scala
+++ src/main/scala/editor/gui/filter/leaf/DevotionFilterPanel.scala
@@ -13,6 +13,27 @@ import javax.swing.JSpinner
 import javax.swing.SpinnerNumberModel
 
 /**
+ * Convenience constructors for [[DevotionFilterPanel]].
+ * @author Alec Roelke
+ */
+object DevotionFilterPanel {
+  /** @return a new, empty [[DevotionFilterPanel]]. */
+  def apply(selector: FilterSelectorPanel) = new DevotionFilterPanel(selector)
+
+  /**
+   * Create a new [[DevotionFilterPanel]] pre-populated with the contents of a filter.
+   * 
+   * @param filter filter to use for populating the new panel
+   * @return a devotion filter panel with contents taken from the filter
+   */
+  def apply(filter: DevotionFilter, selector: FilterSelectorPanel) = {
+    val panel = new DevotionFilterPanel(selector)
+    panel.setFields(filter)
+    panel
+  }
+}
+
+/**
  * A panel for customizing [[DevotionFilter]]s.  The colors to calculate devotion for are selected using a
  * [[ManaSetPanel]], and the comparison type and value to compare with are chosen using a combo box and
  * spinner, respectively.
