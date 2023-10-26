9d3ffb6a8fb1004ac0229acf13a0fcc0c3c6312e
aroelke <alec.roelke@gmail.com>
2023-10-26 15:35:43 -0400

    Fix bug with non-combat card oracle text display

--- src/main/scala/editor/database/card/SingleCard.scala
+++ src/main/scala/editor/database/card/SingleCard.scala
@@ -243,6 +243,7 @@ case class SingleCard(
         case (Some(p), Some(t), None, None) => document.insertString(document.getLength, s"$p/$t\n", textStyle)
         case (None, None, Some(l), None)    => document.insertString(document.getLength, s"[$l]\n", textStyle)
         case (None, None, None, Some(d))    => document.insertString(document.getLength, s"{$d}\n", textStyle)
+        case (None, None, None, None)       => // don't add anything
         case _ => throw IllegalArgumentException(s"Illegal combination of power/toughness, loyalty, defense: $power/$toughness, $loyalty, $defense")
       }
 
